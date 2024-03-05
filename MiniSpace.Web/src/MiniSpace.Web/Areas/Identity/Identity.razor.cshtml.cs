b1e5b5c8758f627e4c5e90b464ba461a020d19cf
Andrii Voznesenskyi <01174367@pw.edu.pl>
2024-03-05 23:31:59 +0100

    (#1) -- adding the basic version of the blazor template  component

---
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/Identity/Identity.razor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiniSpace.Web.Areas.Identity
+{
+    public class Identity_razor : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
