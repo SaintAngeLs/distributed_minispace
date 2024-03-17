93831f24f27986010ee67d4e3571b19c46f23f1f
Andrii Voznesenskyi <01174367@pw.edu.pl>
2024-03-17 21:28:20 +0100

    (#24) (#19) modifying the blazor component for the sign-in

--- MiniSpace.Web/src/MiniSpace.Web/DTO/JwtDto.cs
+++ MiniSpace.Web/src/MiniSpace.Web/DTO/JwtDto.cs
@@ -4,6 +4,7 @@ namespace MiniSpace.Web.DTO
     {
         public string AccessToken { get; set; }
         public string Role { get; set; }
+        public string RefreshToken { get; set; }
         public long Expires { get; set; }
     }
 }
