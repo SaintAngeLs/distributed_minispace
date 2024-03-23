deb590edf0d047669bf1ac2c8add3017227c4601
eggwhat <piotr.padamczyk@gmail.com>
2024-03-23 22:11:21 +0100

    (#30) add contexts

---
+++ MiniSpace.Services.Events/src/MiniSpace.Services.Events.Application/IIdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiniSpace.Services.Events.Application
+{
+    public interface IIdentityContext
+    {
+        Guid Id { get; }
+        string Role { get; }
+        bool IsAuthenticated { get; }
+        bool IsAdmin { get; }
+        IDictionary<string, string> Claims { get; }
+    }
+}
