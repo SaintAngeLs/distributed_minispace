a86571b2aebf898dd14b777fb8fec1fa9eecfb66
Amadeusz Nowak <an2508374@gmail.com>
2024-03-24 00:34:56 +0100

    (#34) add template of Program.cs

---
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Api/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Convey;
+using Convey.Logging;
+using Convey.Types;
+using Convey.WebApi;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using MiniSpace.Services.Students.Application;
+// using MiniSpace.Services.Students.Application.Commands;
+// using MiniSpace.Services.Students.Application.Queries;
+// using MiniSpace.Services.Students.Application.Services;
+using MiniSpace.Services.Students.Infrastructure;
+
+namespace MiniSpace.Services.Students.Api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+            => await WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services
+                    .AddConvey()
+                    .AddWebApi()
+                    .AddApplication()
+                    .AddInfrastructure()
+                    .Build())
+                .Configure(app => app
+                    .UseInfrastructure()
+                    .UseEndpoints(endpoints => endpoints
+                        .Get("", ctx => ctx.Response.WriteAsync(ctx.RequestServices.GetService<AppOptions>().Name))
+                        // .Get<GetStudent, StudentDto>("user/{userId}")
+                        // .Put<UpdateStudent>("user/{userId}")
+                        // .Delete<DeleteStudent>("user/{userId}")
+                    ))
+                .UseLogging()
+                .Build()
+                .RunAsync();
+    }
+}
