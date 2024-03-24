a6ea13452a8cfb0e9134699c65060990d7b3eca0
Amadeusz Nowak <an2508374@gmail.com>
2024-03-24 01:18:11 +0100

    (#34) add Extensions in Application

---
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Extensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Convey;
+using Convey.CQRS.Commands;
+using Convey.CQRS.Events;
+
+namespace MiniSpace.Services.Students.Application
+{
+    public static class Extensions
+    {
+        public static IConveyBuilder AddApplication(this IConveyBuilder builder)
+            => builder
+                .AddCommandHandlers()
+                .AddEventHandlers()
+                .AddInMemoryCommandDispatcher()
+                .AddInMemoryEventDispatcher();
+    }    
+}
