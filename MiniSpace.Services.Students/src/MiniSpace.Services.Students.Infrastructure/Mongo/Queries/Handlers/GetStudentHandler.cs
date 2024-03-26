7846309006dd97ccd8f70811cdd7748fb099bbc8
Amadeusz Nowak <an2508374@gmail.com>
2024-03-26 20:26:18 +0100

    (#34) add GetStudent handler for Mongo

---
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Infrastructure/Mongo/Queries/Handlers/GetStudentHandler.cs
@@ -0,0 +1,25 @@
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using MiniSpace.Services.Students.Application.Dto;
+using MiniSpace.Services.Students.Application.Queries;
+using MiniSpace.Services.Students.Infrastructure.Mongo.Documents;
+
+namespace MiniSpace.Services.Students.Infrastructure.Mongo.Queries.Handlers
+{
+    public class GetStudentHandler : IQueryHandler<GetStudent, StudentDto>
+    {
+        private readonly IMongoRepository<StudentDocument, Guid> _studentRepository;
+
+        public GetStudentHandler(IMongoRepository<StudentDocument, Guid> studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        
+        public async Task<StudentDto> HandleAsync(GetStudent query, CancellationToken cancellationToken)
+        {
+            var document = await _studentRepository.GetAsync(p => p.Id == query.StudentId);
+
+            return document?.AsDto();
+        }
+    }    
+}
