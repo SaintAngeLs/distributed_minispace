c44504975453e8c3c167cdb5d238e8369d1afdea
Amadeusz Nowak <an2508374@gmail.com>
2024-03-27 15:09:14 +0100

    (#34) update fields in SignedUp external event

--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Events/External/SignedUp.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Events/External/SignedUp.cs
@@ -7,16 +7,12 @@ namespace MiniSpace.Services.Students.Application.Events.External
     public class SignedUp : IEvent
     {
         public Guid UserId { get; }
-        public string Username { get; }
-        public string Password { get; }
         public string Email { get; }
         public string Role { get; }
         
-        public SignedUp(Guid userId, string username, string password, string email, string role)
+        public SignedUp(Guid userId, string email, string role)
         {
             UserId = userId;
-            Username = username;
-            Password = password;
             Email = email;
             Role = role;
         }
