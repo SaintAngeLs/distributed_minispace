34883929959031fbe31f3ee10690f876e4698b5e
Amadeusz Nowak <an2508374@gmail.com>
2024-04-07 18:34:45 +0200

    (#45) update IdentityService to store auth data

--- MiniSpace.Web/src/MiniSpace.Web/Areas/Identity/IIdentityService.cs
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/Identity/IIdentityService.cs
@@ -6,8 +6,10 @@ namespace MiniSpace.Web.Areas.Identity
 {
     public interface IIdentityService
     {
-        Task<UserDto> GetAccountAsync(string jwt);
+        bool IsAuthenticated { get; }
+        Task<UserDto> GetAccountAsync();
         Task SignUpAsync(string email, string password, string role = "user", IEnumerable<string> permissions = null);
         Task<JwtDto> SignInAsync(string email, string password);
+        void Logout();
     }
 }
