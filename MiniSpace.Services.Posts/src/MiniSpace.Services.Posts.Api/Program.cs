6875a87f931b2dc105bef0130d8d88f93effbd41
Amadeusz Nowak <an2508374@gmail.com>
2024-04-09 21:58:24 +0200

    (#32) update Program.cs

--- MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Api/Program.cs
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Api/Program.cs
@@ -1,25 +1,43 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Convey;
+using Convey.Logging;
+using Convey.Types;
+using Convey.WebApi;
+using Convey.WebApi.CQRS;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using MiniSpace.Services.Posts.Application;
+using MiniSpace.Services.Posts.Application.Commands;
+using MiniSpace.Services.Posts.Application.Dto;
+using MiniSpace.Services.Posts.Application.Queries;
+using MiniSpace.Services.Posts.Infrastructure;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace MiniSpace.Services.Posts.Api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static async Task Main(string[] args)
+            => await WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services
+                    .AddConvey()
+                    .AddWebApi()
+                    .AddApplication()
+                    .AddInfrastructure()
+                    .Build())
+                .Configure(app => app
+                    .UseInfrastructure()
+                    .UseDispatcherEndpoints(endpoints => endpoints
+                        .Get("", ctx => ctx.Response.WriteAsync(ctx.RequestServices.GetService<AppOptions>().Name))
+                        .Get<GetPosts, IEnumerable<PostDto>>("posts")
+                        .Put<UpdatePost>("posts/{postId}")
+                        .Delete<DeletePost>("posts/{postId}")
+                        .Post<CreatePost>("posts")
+                    ))
+                .UseLogging()
+                .Build()
+                .RunAsync();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
