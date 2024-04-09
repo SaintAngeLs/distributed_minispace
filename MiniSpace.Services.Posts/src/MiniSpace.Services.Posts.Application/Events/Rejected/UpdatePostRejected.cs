feeada3ddeabc6026cb8a5865ba01c29ea4cf684
Amadeusz Nowak <an2508374@gmail.com>
2024-04-09 23:17:25 +0200

    (#32) add basic and rejected events

---
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Events/Rejected/UpdatePostRejected.cs
@@ -0,0 +1,18 @@
+using Convey.CQRS.Events;
+
+namespace MiniSpace.Services.Posts.Application.Events.Rejected
+{
+    public class UpdatePostRejected : IRejectedEvent
+    {
+        public Guid PostId { get; }
+        public string Reason { get; }
+        public string Code { get; }
+
+        public UpdatePostRejected(Guid postId, string reason, string code)
+        {
+            PostId = postId;
+            Reason = reason;
+            Code = code;
+        }
+    }    
+}
