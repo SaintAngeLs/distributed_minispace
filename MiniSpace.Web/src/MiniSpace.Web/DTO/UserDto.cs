5b364ef444162fcdfa9a82fc9b277b0ee14e2c62
eggwhat <piotr.padamczyk@gmail.com>
2024-04-14 16:24:02 +0200

    (#52) update signup in frontend

--- MiniSpace.Web/src/MiniSpace.Web/DTO/UserDto.cs
+++ MiniSpace.Web/src/MiniSpace.Web/DTO/UserDto.cs
@@ -5,6 +5,7 @@ namespace MiniSpace.Web.DTO
     public class UserDto
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
         public DateTime CreatedAt { get; set; }
