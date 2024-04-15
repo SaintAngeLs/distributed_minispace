164bda167b75376843831abecaab0459404891b0
eggwhat <piotr.padamczyk@gmail.com>
2024-04-15 12:05:05 +0200

    (#52)(#53) resolve merge conflict

--- MiniSpace.Web/src/MiniSpace.Web/Areas/Identity/IIdentityService.cs
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/Identity/IIdentityService.cs
@@ -5,9 +5,12 @@ using MiniSpace.Web.DTO;
 namespace MiniSpace.Web.Areas.Identity
 {
     public interface IIdentityService
-    {
-        Task<UserDto> GetAccountAsync(string jwt);
+    { 
+        public JwtDto JwtDto { get; }
+        bool IsAuthenticated { get; }
+        Task<UserDto> GetAccountAsync();
         Task SignUpAsync(string firstName, string lastName, string email, string password, string role = "user", IEnumerable<string> permissions = null);
         Task<JwtDto> SignInAsync(string email, string password);
+        void Logout();
     }
 }
