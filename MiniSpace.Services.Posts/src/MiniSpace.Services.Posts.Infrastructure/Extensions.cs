eca528545cdbb04e77410b21631d4791a430b48c
Amadeusz Nowak <an2508374@gmail.com>
2024-04-16 00:19:11 +0200

    (#32) add services in Infrastructure

--- MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Infrastructure/Extensions.cs
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Infrastructure/Extensions.cs
@@ -48,5 +48,36 @@ namespace MiniSpace.Services.Posts.Infrastructure
             => accessor.HttpContext?.Request.Headers.TryGetValue("Correlation-Context", out var json) is true
                 ? JsonConvert.DeserializeObject<CorrelationContext>(json.FirstOrDefault())
                 : null;
+        
+        internal static IDictionary<string, object> GetHeadersToForward(this IMessageProperties messageProperties)
+        {
+            const string sagaHeader = "Saga";
+            if (messageProperties?.Headers is null || !messageProperties.Headers.TryGetValue(sagaHeader, out var saga))
+            {
+                return null;
+            }
+        
+            return saga is null
+                ? null
+                : new Dictionary<string, object>
+                {
+                    [sagaHeader] = saga
+                };
+        }
+        
+        internal static string GetSpanContext(this IMessageProperties messageProperties, string header)
+        {
+            if (messageProperties is null)
+            {
+                return string.Empty;
+            }
+        
+            if (messageProperties.Headers.TryGetValue(header, out var span) && span is byte[] spanBytes)
+            {
+                return Encoding.UTF8.GetString(spanBytes);
+            }
+        
+            return string.Empty;
+        }
     }    
 }
