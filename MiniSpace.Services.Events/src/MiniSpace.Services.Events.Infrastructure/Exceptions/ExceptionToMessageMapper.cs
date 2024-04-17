6b64123cc0db0e0524f440718aae6f9bfca405e5
eggwhat <piotr.padamczyk@gmail.com>
2024-04-18 00:12:54 +0200

    (#30) update exception mappers

--- MiniSpace.Services.Events/src/MiniSpace.Services.Events.Infrastructure/Exceptions/ExceptionToMessageMapper.cs
+++ MiniSpace.Services.Events/src/MiniSpace.Services.Events.Infrastructure/Exceptions/ExceptionToMessageMapper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Convey.MessageBrokers.RabbitMQ;
+using MiniSpace.Services.Events.Application.Events.Rejected;
+using MiniSpace.Services.Events.Application.Exceptions;
 
 namespace MiniSpace.Services.Events.Infrastructure.Exceptions
 {
@@ -8,7 +10,12 @@ namespace MiniSpace.Services.Events.Infrastructure.Exceptions
         public object Map(Exception exception, object message)
             => exception switch
             {
-                
+                // TODO: Add more exceptions
+                AuthorizedUserIsNotAnOrganizerException ex => new AddEventRejected(ex.UserId, ex.Message, ex.Code),
+                OrganizerCannotAddEventForAnotherOrganizerException ex => new AddEventRejected(ex.OrganizerId, ex.Message, ex.Code),
+                InvalidEventCategoryException ex => new AddEventRejected(Guid.Empty, ex.Message, ex.Code),
+                InvalidEventDateTimeException ex => new AddEventRejected(Guid.Empty, ex.Message, ex.Code),
+                InvalidEventDateTimeOrderException ex => new AddEventRejected(Guid.Empty, ex.Message, ex.Code),
                 _ => null,
             };
     }
