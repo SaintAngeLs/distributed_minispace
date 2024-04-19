be6d04dd3fd2be7ac9875feed255ee90b9e5047e
eggwhat <piotr.padamczyk@gmail.com>
2024-04-20 00:39:52 +0200

    (#57) add events service

---
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/Events/IEventsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MiniSpace.Web.DTO;
+using MiniSpace.Web.DTO.Wrappers;
+
+namespace MiniSpace.Web.Areas.Events
+{
+    public interface IEventsService
+    {
+        Task<EventDto> GetEventAsync(Guid eventId);
+        Task<PagedResponseDto<IEnumerable<EventDto>>> GetStudentEventsAsync(Guid studentId, int numberOfResults);
+        Task AddEventAsync(Guid eventId, string name, Guid organizerId, DateTime startDate, DateTime endDate,
+            string buildingName, string street, string buildingNumber, string apartmentNumber, string city,
+            string zipCode, string description, int capacity, decimal fee, string category, DateTime publishDate);
+        Task SignUpToEventAsync(Guid eventId, Guid studentId);
+        Task ShowInterestInEventAsync(Guid eventId, Guid studentId);
+        Task RateEventAsync(Guid eventId, int rating, Guid studentId);
+        Task<PagedResponseDto<IEnumerable<EventDto>>> SearchEventsAsync(string name, string organizer, 
+            DateTime dateFrom, DateTime dateTo, PageableDto pageable);
+    }
+}
---
+++ MiniSpace.Web/src/MiniSpace.Web/DTO/OrganizerDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniSpace.Web.DTO
+{
+    public class OrganizerDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Organization { get; set; }
+    }
+}
