57ac36c7ecdc3545e227ef028f1cbba8cbe54c99
eggwhat <piotr.padamczyk@gmail.com>
2024-04-20 19:47:03 +0200

    (#57) add name and email fields for identity service

--- MiniSpace.Web/src/MiniSpace.Web/Areas/Identity/IdentityService.cs
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/Identity/IdentityService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
 using MiniSpace.Web.DTO;
 using MiniSpace.Web.HttpClients;
@@ -8,12 +9,16 @@ namespace MiniSpace.Web.Areas.Identity
     class IdentityService : IIdentityService
     {
         private readonly IHttpClient _httpClient;
+        private readonly JwtSecurityTokenHandler _jwtHandler;
         public JwtDto JwtDto { get; private set; }
+        public string Name {get; private set; }
+        public string Email {get; private set; }
         public bool IsAuthenticated { get; private set; }
         
         public IdentityService(IHttpClient httpClient)
         {
             _httpClient = httpClient;
+            _jwtHandler = new JwtSecurityTokenHandler();
         }
         
         public Task<UserDto> GetAccountAsync()
@@ -28,13 +33,21 @@ namespace MiniSpace.Web.Areas.Identity
         public async Task<JwtDto> SignInAsync(string email, string password)
         {
             JwtDto = await _httpClient.PostAsync<object, JwtDto>("identity/sign-in", new {email, password});
+            
+            var jwtToken = _jwtHandler.ReadJwtToken(JwtDto.AccessToken);
+            var payload = jwtToken.Payload;
+            Name = (string)payload["name"];
+            Email = (string)payload["e-mail"];
             IsAuthenticated = true;
+            
             return JwtDto;
         }
 
         public void Logout()
         {
             JwtDto = null;
+            Name = null;
+            Email = null;
             IsAuthenticated = false;
         }
     }
