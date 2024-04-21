cc7464da392c13f3799f9ab1f58d9b1d3b1b419a
olegkiprik <oleg.kiprik@proton.me>
2024-04-21 10:09:21 +0200

    (#48) Add domain event, exception and aggregates in Core

---
+++ MiniSpace.Services.Reactions/src/MiniSpace.Services.Reactions.Core/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+namespace MiniSpace.Services.Reactions.Core.Exceptions
+{
+    public abstract class DomainException : Exception
+    {
+        public virtual string Code { get; }
+
+        protected DomainException(string message) : base(message)
+        {
+        }
+    }
+}
