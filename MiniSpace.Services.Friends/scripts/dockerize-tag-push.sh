048bc82fd72217958024efccb593a0d212c761e1
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-04-21 16:36:31 +0200

    (#80) service appsetting update and operating scripts update

--- MiniSpace.Services.Friends/scripts/dockerize-tag-push.sh
+++ MiniSpace.Services.Friends/scripts/dockerize-tag-push.sh
@@ -4,8 +4,8 @@ export ASPNETCORE_ENVIRONMENT=docker
 
 cd ..
 
-docker build -t minispace.services.students:latest .
+docker build -t minispace.services.friends:latest .
 
-docker tag minispace.services.students:latest adrianvsaint/minispace.services.students:latest
+docker tag minispace.services.friends:latest adrianvsaint/minispace.services.friends:latest
 
-docker push adrianvsaint/minispace.services.students:latest
+docker push adrianvsaint/minispace.services.friends:latest
