640b832f6481cdcbd57a770f092d1a433367edf3
eggwhat <piotr.padamczyk@gmail.com>
2024-04-21 16:03:00 +0200

    (#79) add event mapper service

---
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/Http/ErrorMapperService.cs
@@ -0,0 +1,18 @@
+﻿using MiniSpace.Web.HttpClients;
+
+namespace MiniSpace.Web.Areas.Http
+{
+    public class ErrorMapperService : IErrorMapperService
+    {
+        public string MapError(ErrorMessage error)
+        {
+            switch (error.Code)
+            {
+                case "invalid_credentials":
+                    return "Failed to sign in. Please check your credentials and try again.";
+                default:
+                    return "Something went wrong. Please try again later.";
+            }
+        }
+    }
+}
