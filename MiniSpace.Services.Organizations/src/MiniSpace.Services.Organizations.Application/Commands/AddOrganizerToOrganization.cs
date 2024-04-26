475851560d9d56b2ed9ddd4833077eeb5bdd108c
eggwhat <piotr.padamczyk@gmail.com>
2024-04-27 01:44:41 +0200

    (#87) fix endpoint for adding an organizer

--- MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Application/Commands/AddOrganizerToOrganization.cs
+++ MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Application/Commands/AddOrganizerToOrganization.cs
@@ -4,7 +4,13 @@ namespace MiniSpace.Services.Organizations.Application.Commands
 {
     public class AddOrganizerToOrganization: ICommand
     {
-        public Guid OrganizerId { get; set; }
         public Guid OrganizationId { get; set; }
+        public Guid OrganizerId { get; set; }
+        
+        public AddOrganizerToOrganization(Guid organizationId, Guid organizerId)
+        {
+            OrganizationId = organizationId;
+            OrganizerId = organizerId;
+        }
     }
 }
