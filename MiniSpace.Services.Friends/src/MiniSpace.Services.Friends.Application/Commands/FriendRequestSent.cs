8317916808c2e28ac26309849e64d5f89c8f7db2
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-04-26 16:10:11 +0200

    (#82)  friend request send command update

--- MiniSpace.Services.Friends/src/MiniSpace.Services.Friends.Application/Commands/FriendRequestSent.cs
+++ MiniSpace.Services.Friends/src/MiniSpace.Services.Friends.Application/Commands/FriendRequestSent.cs
@@ -1,17 +1,18 @@
 
-using Convey.CQRS.Commands;
+// using Convey.CQRS.Commands;
+// using Convey.CQRS.Events;
 
-namespace MiniSpace.Services.Friends.Core.Events
-{
-    public class FriendRequestSent : ICommand
-    {
-        public Guid InviterId { get; }
-        public Guid InviteeId { get; }
+// namespace MiniSpace.Services.Friends.Application.Events
+// {
+//     public class FriendRequestSent : IEvent
+//     {
+//         public Guid InviterId { get; }
+//         public Guid InviteeId { get; }
 
-        public FriendRequestSent(Guid inviterId, Guid inviteeId)
-        {
-            InviterId = inviterId;
-            InviteeId = inviteeId;
-        }
-    }
-}
+//         public FriendRequestSent(Guid inviterId, Guid inviteeId)
+//         {
+//             InviterId = inviterId;
+//             InviteeId = inviteeId;
+//         }
+//     }
+// }
