eacfecdd7114551faf1f3c2835c6cf2132c68c16
olegkiprik <oleg.kiprik@proton.me>
2024-05-06 01:50:09 +0200

    (#48) Remove unneeded OrganizerId in EventDocument

--- MiniSpace.Services.Reactions/src/MiniSpace.Services.Reactions.Infrastructure/Mongo/Documents/EventDocument.cs
+++ MiniSpace.Services.Reactions/src/MiniSpace.Services.Reactions.Infrastructure/Mongo/Documents/EventDocument.cs
@@ -5,6 +5,5 @@ namespace MiniSpace.Services.Reactions.Infrastructure.Mongo.Documents
     public class EventDocument : IIdentifiable<Guid>
     {
         public Guid Id { get; set; }
-        public Guid OrganizerId {get;set;}
     }    
 }
