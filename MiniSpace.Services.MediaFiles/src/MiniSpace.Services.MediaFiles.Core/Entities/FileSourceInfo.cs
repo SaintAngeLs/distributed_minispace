ff12d948418e8fbbc2790818c15e2769050bf324
eggwhat <piotr.padamczyk@gmail.com>
2024-05-08 18:49:56 +0200

    (#92) add wrapper class for storing media source info

---
+++ MiniSpace.Services.MediaFiles/src/MiniSpace.Services.MediaFiles.Core/Entities/FileSourceInfo.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace MiniSpace.Services.MediaFiles.Core.Entities
+{
+    public class FileSourceInfo: AggregateRoot
+    {
+        public Guid SourceId { get; set; }
+        public ContextType SourceType { get; set; }
+        public ObjectId FileId { get; set; }
+    }
+}
