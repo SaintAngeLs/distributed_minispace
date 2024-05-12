476c15e4ca3c0ae3ea812ff26a97828e213bd940
eggwhat <piotr.padamczyk@gmail.com>
2024-05-12 14:16:35 +0200

    (#30) remove redundant field in event

--- MiniSpace.Services.Events/src/MiniSpace.Services.Events.Application/Events/EventDeleted.cs
+++ MiniSpace.Services.Events/src/MiniSpace.Services.Events.Application/Events/EventDeleted.cs
@@ -4,9 +4,8 @@ using Convey.CQRS.Events;
 
 namespace MiniSpace.Services.Events.Application.Events
 {
-    public class EventDeleted(Guid eventId, IEnumerable<Guid> mediaFilesIds) : IEvent
+    public class EventDeleted(Guid eventId) : IEvent
     {
         public Guid EventId { get; set; } = eventId;
-        public IEnumerable<Guid> MediaFilesIds { get; set; } = mediaFilesIds;
     }
 }
