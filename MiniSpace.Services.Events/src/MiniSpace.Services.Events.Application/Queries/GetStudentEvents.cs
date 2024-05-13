d95d8607525bd0efb66671089f76ca269f185a0c
eggwhat <piotr.padamczyk@gmail.com>
2024-05-13 22:14:40 +0200

    (#30) add page field for GetStudentEvents query

--- MiniSpace.Services.Events/src/MiniSpace.Services.Events.Application/Queries/GetStudentEvents.cs
+++ MiniSpace.Services.Events/src/MiniSpace.Services.Events.Application/Queries/GetStudentEvents.cs
@@ -9,6 +9,7 @@ namespace MiniSpace.Services.Events.Application.Queries
     public class GetStudentEvents : IQuery<PagedResponse<IEnumerable<EventDto>>>
     {
         public Guid StudentId { get; set; }
+        public int Page { get; set; }
         public int NumberOfResults { get; set; }
     }
 }
--- MiniSpace.Services.Events/src/MiniSpace.Services.Events.Infrastructure/Mongo/Queries/Handlers/GetStudentEventsHandler.cs
+++ MiniSpace.Services.Events/src/MiniSpace.Services.Events.Infrastructure/Mongo/Queries/Handlers/GetStudentEventsHandler.cs
@@ -43,12 +43,12 @@ namespace MiniSpace.Services.Events.Infrastructure.Mongo.Queries.Handlers
             var studentEvents = await _studentsServiceClient.GetAsync(query.StudentId);
             var studentEventIds = studentEvents.InterestedInEvents.Union(studentEvents.SignedUpEvents).ToList();
             
-            var result = await _eventRepository.BrowseEventsAsync(1, query.NumberOfResults,
+            var result = await _eventRepository.BrowseEventsAsync(query.Page, query.NumberOfResults,
                 string.Empty, string.Empty, DateTime.MinValue, DateTime.MinValue, null, null,
                 Enumerable.Empty<Guid>(), null, Enumerable.Empty<string>(), "asc", studentEventIds);
 
             return new PagedResponse<IEnumerable<EventDto>>(result.Item1.Select(e => new EventDto(e, identity.Id)), 
-                result.Item2, result.Item3, result.Item4, result.Item5);;
+                result.pageNumber, result.pageSize, result.totalPages, result.totalElements);
         }
     }
 }
