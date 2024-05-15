c4ffff75d9bc2378420425e97a0bcbdd53ebe705
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-05-15 23:39:27 +0200

    (#125) adding the infrastucture clined for the friends service

---
+++ MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Infrastructure/Services/Clients/FriendsServiceClient.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniSpace.Services.Notifications.Infrastructure.Services.Clients
+{
+    public class FriendsServiceClient :  IFriendsServiceClient
+    {
+        private readonly IHttpClient _httpClient;
+        private readonly string _url;
+
+        public FriendsServiceClient(IHttpClient httpClient, HttpClientOptions options)
+        {
+            _httpClient = httpClient;
+            _url = options.Services["friends"];
+        }
+
+        public Task<IEnumerable<FriendDto>> GetAsync(Guid studentId)
+            => _httpClient.GetAsync<IEnumerable<FriendDto>>($"{_url}/friends/{studentId}");
+    }
+}
