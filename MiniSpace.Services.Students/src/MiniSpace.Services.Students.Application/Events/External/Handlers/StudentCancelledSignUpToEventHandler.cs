10fa3b95409fe87edd2e3dff0fcf84b72f1f6c15
eggwhat <piotr.padamczyk@gmail.com>
2024-05-17 16:16:53 +0200

    (#151) add missing events for cancelling event interest and sign up

---
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Events/External/Handlers/StudentCancelledSignUpToEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using Convey.CQRS.Events;
+using MiniSpace.Services.Students.Application.Exceptions;
+using MiniSpace.Services.Students.Application.Services;
+using MiniSpace.Services.Students.Core.Repositories;
+
+namespace MiniSpace.Services.Students.Application.Events.External.Handlers
+{
+    public class StudentCancelledSignUpToEventHandler : IEventHandler<StudentCancelledSignUpToEvent>
+    {
+        private readonly IStudentRepository _studentRepository;
+        private readonly IEventMapper _eventMapper;
+        private readonly IMessageBroker _messageBroker;
+
+        public StudentCancelledSignUpToEventHandler(IStudentRepository studentRepository,
+            IEventMapper eventMapper, IMessageBroker messageBroker)
+        {
+            _studentRepository = studentRepository;
+            _eventMapper = eventMapper;
+            _messageBroker = messageBroker;
+        }
+        
+        public async Task HandleAsync(StudentCancelledSignUpToEvent @event, CancellationToken cancellationToken)
+        {
+            var student = await _studentRepository.GetAsync(@event.StudentId);
+            if (student is null)
+            {
+                throw new StudentNotFoundException(@event.StudentId);
+            }
+            
+            student.RemoveSignedUpEvent(@event.EventId);
+            await _studentRepository.UpdateAsync(student);
+            
+            var events = _eventMapper.MapAll(student.Events);
+            await _messageBroker.PublishAsync(events.ToArray());
+        }
+    }    
+}
