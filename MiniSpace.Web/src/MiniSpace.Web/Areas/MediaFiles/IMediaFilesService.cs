cfb907aadcbc6adeff99cf7e0124353a00c705a2
eggwhat <piotr.padamczyk@gmail.com>
2024-05-18 16:26:46 +0200

    (#92)(#57) add media files service

---
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/MediaFiles/IMediaFilesService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using MiniSpace.Web.DTO;
+using MiniSpace.Web.HttpClients;
+
+namespace MiniSpace.Web.Areas.MediaFiles
+{
+    public interface IMediaFilesService
+    {
+        public Task<FileDto> GetFileAsync(Guid fileId);
+        public Task<FileDto> GetOriginalFileAsync(Guid fileId);
+        public Task<HttpResponse<object>> UploadMediaFileAsync(Guid sourceId, string sourceType, Guid uploaderId,
+            string fileName, string fileContentType, string base64Content);
+        public Task DeleteMediaFileAsync(Guid fileId);
+    }
+}
---
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/MediaFiles/MediaFilesService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using MiniSpace.Web.Areas.Identity;
+using MiniSpace.Web.DTO;
+using MiniSpace.Web.HttpClients;
+
+namespace MiniSpace.Web.Areas.MediaFiles
+{
+    public class MediaFilesService : IMediaFilesService
+    {
+        private readonly IHttpClient _httpClient;
+        private readonly IIdentityService _identityService;
+        
+        public MediaFilesService(IHttpClient httpClient, IIdentityService identityService)
+        {
+            _httpClient = httpClient;
+            _identityService = identityService;
+        }
+        
+        public Task<FileDto> GetFileAsync(Guid fileId)
+        {
+            return _httpClient.GetAsync<FileDto>($"media-files/{fileId}");
+        }
+        
+        public Task<FileDto> GetOriginalFileAsync(Guid fileId)
+        {
+            return _httpClient.GetAsync<FileDto>($"media-files/{fileId}/original");
+        }
+
+        public Task<HttpResponse<object>> UploadMediaFileAsync(Guid sourceId, string sourceType, Guid uploaderId, string fileName,
+            string fileContentType, string base64Content)
+        {
+            _httpClient.SetAccessToken(_identityService.JwtDto.AccessToken);
+            return _httpClient.PostAsync<object, object>("media-files", new {sourceId, sourceType, uploaderId, 
+                fileName, fileContentType, base64Content });
+        }
+        
+        public Task DeleteMediaFileAsync(Guid fileId)
+        {
+            _httpClient.SetAccessToken(_identityService.JwtDto.AccessToken);
+            return _httpClient.DeleteAsync($"media-files/{fileId}");
+        }
+        
+    }
+}
