665bcd9a7c3f9532af054fdd46e4bca77a2453f6
Amadeusz Nowak <an2508374@gmail.com>
2024-05-19 00:23:06 +0200

    (#57) add option of creating and deleting reactions for event

--- MiniSpace.Web/src/MiniSpace.Web/DTO/Enums/ReactionType.cs
+++ MiniSpace.Web/src/MiniSpace.Web/DTO/Enums/ReactionType.cs
@@ -8,4 +8,20 @@ namespace MiniSpace.Web.DTO.Enums
         ItWasOkay,
         HateIt
     }
+
+    public static class ReactionTypeExtensions
+    {
+        public static string GetReactionText(ReactionType? reactionType)
+        {
+            return reactionType switch
+            {
+                ReactionType.LoveIt => "Love it!",
+                ReactionType.LikeIt => "Like it.",
+                ReactionType.Wow => "Wow!",
+                ReactionType.ItWasOkay => "It was okay.",
+                ReactionType.HateIt => "Hate it!",
+                _ => "No reactions!"
+            };
+        }   
+    }
 }
