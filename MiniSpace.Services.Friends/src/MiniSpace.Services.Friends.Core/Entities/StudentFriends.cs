7bf23038e6845cfa9a42622cc5ae938452c14e78
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-05-19 15:27:34 +0200

    (#125) friends core udpate

---
+++ MiniSpace.Services.Friends/src/MiniSpace.Services.Friends.Core/Entities/StudentFriends.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using MiniSpace.Services.Friends.Core.Events;
+using MiniSpace.Services.Friends.Core.Exceptions;
+
+namespace MiniSpace.Services.Friends.Core.Entities
+{
+    public class StudentFriends : AggregateRoot
+    {
+        public Guid StudentId { get; private set; }
+        private List<Friend> _friends;
+        public IEnumerable<Friend> Friends => _friends.AsReadOnly();
+
+        public StudentFriends(Guid studentId)
+        {
+            Id = studentId; 
+            StudentId = studentId;
+            _friends = new List<Friend>();
+        }
+    }
+}
