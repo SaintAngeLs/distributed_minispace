0ac8f284980cdda816abb08a2fb9e86f2c9145eb
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-05-19 15:29:41 +0200

    (#125) interface for the studentFrineds repository

---
+++ MiniSpace.Services.Friends/src/MiniSpace.Services.Friends.Core/Repositories/IStudentFriendsRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MiniSpace.Services.Friends.Core.Entities;
+
+namespace MiniSpace.Services.Friends.Core.Repositories
+{
+    public interface IStudentFriendsRepository
+    {
+        Task<StudentFriends> GetAsync(Guid studentId);
+        Task<IEnumerable<StudentFriends>> GetAllAsync();
+        Task AddAsync(StudentFriends studentFriends);
+        Task UpdateAsync(StudentFriends studentFriends);
+        Task DeleteAsync(Guid studentId);
+        Task<bool> ExistsAsync(Guid studentId);
+        Task<IEnumerable<Friend>> GetFriendsAsync(Guid studentId);
+    }
+}
