9032e88682a984455ec0df8fb1e72736bac7bc43
OJ <Zniwiarz@xd.xd>
2024-05-19 14:50:28 +0200

    Create comment Handler Tests

---
+++ MiniSpace.Services.Comments/tests/MiniSpace.Services.Comments.Application.UnitTests/Commands/Handlers/DeleteCommentHandlerTest.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MiniSpace.Services.Comments.Application.Exceptions;
+using MiniSpace.Services.Comments.Application.Services;
+using MiniSpace.Services.Comments.Core.Entities;
+using MiniSpace.Services.Comments.Core.Repositories;
+using MiniSpace.Services.Comments.Application.Commands.Handlers;
+using MiniSpace.Services.Comments.Application.Commands;
+using MiniSpace.Services.Comments.Infrastructure.Contexts;
+using System.Threading;
+using FluentAssertions;
+
+namespace MiniSpace.Services.Comments.Application.UnitTests.Commands.Handlers
+{
+    public class DeleteCommentHandlerTest
+    {
+        private readonly DeleteCommentHandler _deleteCommentHandler;
+        private readonly Mock<ICommentRepository> _commentRepositoryMock;
+        private readonly Mock<IMessageBroker> _messageBrokerMock;
+        private readonly Mock<IAppContext> _appContextMock;
+
+        public DeleteCommentHandlerTest()
+        {
+            _commentRepositoryMock = new Mock<ICommentRepository>();
+            _messageBrokerMock = new Mock<IMessageBroker>();
+            _appContextMock = new Mock<IAppContext>();
+            _deleteCommentHandler = new DeleteCommentHandler(
+                _commentRepositoryMock.Object,
+                _appContextMock.Object,
+                _messageBrokerMock.Object
+                );
+        }
+    }
+}
