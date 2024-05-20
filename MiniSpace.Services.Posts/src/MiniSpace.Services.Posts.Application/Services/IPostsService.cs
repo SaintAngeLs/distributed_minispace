1b5987ca1946419994d81e4bca7f8d284085151c
eggwhat <piotr.padamczyk@gmail.com>
2024-05-20 17:59:26 +0200

    (#32) add posts service

---
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Services/IPostsService.cs
@@ -0,0 +1,10 @@
+﻿using MiniSpace.Services.Posts.Application.Dto;
+using MiniSpace.Services.Posts.Core.Wrappers;
+
+namespace MiniSpace.Services.Posts.Application.Services
+{
+    public interface IPostsService
+    {
+        Task<PagedResponse<IEnumerable<PostDto>>> BrowsePostsAsync(SearchPosts command);
+    }
+}
---
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Infrastructure/Services/PostsService.cs
@@ -0,0 +1,42 @@
+﻿using MiniSpace.Services.Posts.Application;
+using MiniSpace.Services.Posts.Application.Commands;
+using MiniSpace.Services.Posts.Application.Dto;
+using MiniSpace.Services.Posts.Application.Exceptions;
+using MiniSpace.Services.Posts.Application.Services;
+using MiniSpace.Services.Posts.Core.Wrappers;
+using MiniSpace.Services.Posts.Core.Entities;
+using MiniSpace.Services.Posts.Core.Repositories;
+using MiniSpace.Services.Posts.Infrastructure.Mongo.Documents;
+
+namespace MiniSpace.Services.Posts.Infrastructure.Services
+{
+    public class PostsService : IPostsService
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IAppContext _appContext;
+
+        public PostsService(IPostRepository postRepository, IAppContext appContext)
+        {
+            _postRepository = postRepository;
+            _appContext = appContext;
+        }
+
+        public async Task<PagedResponse<IEnumerable<PostDto>>> BrowsePostsAsync(SearchPosts command)
+        {
+            var identity = _appContext.Identity;
+
+            var pageNumber = command.Pageable.Page < 1 ? 1 : command.Pageable.Page;
+            var pageSize = command.Pageable.Size > 10 ? 10 : command.Pageable.Size;
+
+            var result = await _postRepository.BrowseCommentsAsync(
+                pageNumber, pageSize, command.ContextId, context, command.ParentId,
+                command.Pageable.Sort.SortBy, command.Pageable.Sort.Direction);
+
+            var pagedEvents = new PagedResponse<IEnumerable<PostDto>>(
+                result.comments.Select(p => new PostDto(p)),
+                result.pageNumber, result.pageSize, result.totalPages, result.totalElements);
+
+            return pagedEvents;
+        }
+    }
+}
