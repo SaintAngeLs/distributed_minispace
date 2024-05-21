c3bfab6c5f3c07b74b07a5aef68039dcacfdbbb1
Amadeusz Nowak <an2508374@gmail.com>
2024-05-21 18:55:51 +0200

    (#57) update way of getting subcomments

--- MiniSpace.Web/src/MiniSpace.Web/DTO/CommentDto.cs
+++ MiniSpace.Web/src/MiniSpace.Web/DTO/CommentDto.cs
@@ -18,6 +18,7 @@ namespace MiniSpace.Web.DTO
         public DateTime LastReplyAt { get; set; }
         public int RepliesCount { get; set; }
         public bool IsDeleted { get; set; }
-        public HashSet<CommentDto> Children { get; set; }
+        public bool CanExpand { get; set; }
+        public bool IsExpanded { get; set; }
     }    
 }
