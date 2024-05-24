314243347947b5ca01f34754ae71c64fe2b5efaf
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-05-24 14:54:16 +0200

    (#125) get notification by user id implementation

--- MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Exceptions/NotificationNotFoundException.cs
+++ MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Exceptions/NotificationNotFoundException.cs
@@ -4,11 +4,18 @@ namespace MiniSpace.Services.Notifications.Application.Exceptions
     {
         public override string Code { get; } = "notification_not_found";
         public Guid NotificationId { get; }
+        
 
         public NotificationNotFoundException(Guid notificationId)
             : base($"Notification with ID: {notificationId} was not found.")
         {
             NotificationId = notificationId;
+        }
+
+        public NotificationNotFoundException(Guid userId, string message)
+            : base(message)
+        {
+            NotificationId = userId;
         }
     }
 }
