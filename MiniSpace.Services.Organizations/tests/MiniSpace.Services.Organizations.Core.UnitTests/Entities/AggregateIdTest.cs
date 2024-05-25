8c5a33df99db2456b4e27f6ab28f928baa693746
OJ <Zniwiarz@xd.xd>
2024-05-25 18:57:17 +0200

    (#170) Core: Etities AggregatedId an Organizations tests

---
+++ MiniSpace.Services.Organizations/tests/MiniSpace.Services.Organizations.Core.UnitTests/Entities/AggregateIdTest.cs
@@ -0,0 +1,38 @@
+﻿using MiniSpace.Services.Organizations.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MiniSpace.Services.Organizations.Core.UnitTests.Entities
+{
+    public class AggregateIdTest
+    {
+        [Fact]
+        public void AggregateId_CreatedTwice_ShuldBeDiffrent()
+        {
+            // Arrange & Act
+            var id1 = new AggregateId();
+            var id2 = new AggregateId();
+
+            // Assert
+            Assert.NotEqual(id1.Value, id2.Value);
+        }
+
+        [Fact]
+        public void AggregateId_CreatedTwiceSameGuid_ShuldBeSame()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var id1 = new AggregateId(id);
+            var id2 = new AggregateId(id);
+
+            // Assert
+            Assert.Equal(id1.Value, id2.Value);
+        }
+    }
+}
