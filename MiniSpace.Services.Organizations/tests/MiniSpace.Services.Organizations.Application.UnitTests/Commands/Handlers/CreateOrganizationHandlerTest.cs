77a86db6136a5ae52f08ee9215de4f6b57e24885
OJ <Zniwiarz@xd.xd>
2024-05-25 18:53:53 +0200

    (#170) Application: Commands handlers tests

---
+++ MiniSpace.Services.Organizations/tests/MiniSpace.Services.Organizations.Application.UnitTests/Commands/Handlers/CreateOrganizationHandlerTest.cs
@@ -0,0 +1,159 @@
+﻿using Xunit;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using FluentAssertions;
+using MiniSpace.Services.Organizations.Application.Commands.Handlers;
+using MiniSpace.Services.Organizations.Core.Repositories;
+using MiniSpace.Services.Organizations.Application.Services;
+using MiniSpace.Services.Organizations.Core.Entities;
+using MiniSpace.Services.Organizations.Infrastructure.Contexts;
+using MiniSpace.Services.Organizations.Application.Commands;
+using MiniSpace.Services.Organizations.Application.Exceptions;
+
+namespace MiniSpace.Services.Organizations.Application.UnitTests.Commands.Handlers
+{
+    public class CreateOrganizationHandlerTest
+    {
+        private readonly CreateOrganizationHandler _createOrganizationHandler;
+        private readonly Mock<IOrganizationRepository> _organizationRepositoryMock;
+        private readonly Mock<IAppContext> _appContextMock;
+        private readonly Mock<IMessageBroker> _messageBrokerMock;
+
+        public CreateOrganizationHandlerTest()
+        {
+            _organizationRepositoryMock = new Mock<IOrganizationRepository>();
+            _appContextMock = new Mock<IAppContext>();
+            _messageBrokerMock = new Mock<IMessageBroker>();
+            _createOrganizationHandler = new CreateOrganizationHandler(
+                _organizationRepositoryMock.Object,
+                _appContextMock.Object,
+                _messageBrokerMock.Object
+                );
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithValidorganizationAndAuthorised_ShouldUpdateRootOrganization()
+        {
+            // Arrange
+            var rootOrganizationId = Guid.NewGuid();
+            var organizationId = Guid.NewGuid();
+            var parentId = Guid.NewGuid();
+            var comand = new CreateOrganization(organizationId, "this", rootOrganizationId, parentId);
+
+            var organization = new Organization(organizationId, "this");
+            var parent = new Organization(parentId, "parent", new List<Organizer>() { }, new List<Organization>() { });
+            var rootOrganization = new Organization(rootOrganizationId, "root", new List<Organizer>() { }, new List<Organization>() { parent });
+            var identityContext = new IdentityContext(Guid.NewGuid().ToString(), "Admin", true, new Dictionary<string, string>());
+
+            _appContextMock.Setup(ctx => ctx.Identity).Returns(identityContext);
+            _organizationRepositoryMock.Setup(repo => repo.GetAsync(comand.RootId)).ReturnsAsync(rootOrganization);
+            
+            var cancelationToken = new CancellationToken();
+
+            // Act
+            await _createOrganizationHandler.HandleAsync(comand, cancelationToken);
+
+            // Assert
+            _organizationRepositoryMock.Verify(repo => repo.UpdateAsync(rootOrganization), Times.Once());
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithoutAdminRole_ShouldThrowUnauthorizedAccessException()
+        {
+            // Arrange
+            var rootOrganizationId = Guid.NewGuid();
+            var organizationId = Guid.NewGuid();
+            var parentId = Guid.NewGuid();
+            var comand = new CreateOrganization(organizationId, "this", rootOrganizationId, parentId);
+
+            var organization = new Organization(organizationId, "this");
+            var parent = new Organization(parentId, "parent", new List<Organizer>() { }, new List<Organization>() { });
+            var rootOrganization = new Organization(rootOrganizationId, "root", new List<Organizer>() { }, new List<Organization>() { parent });
+            var identityContext = new IdentityContext(Guid.NewGuid().ToString(), "", true, new Dictionary<string, string>());
+
+            _appContextMock.Setup(ctx => ctx.Identity).Returns(identityContext);
+            _organizationRepositoryMock.Setup(repo => repo.GetAsync(comand.RootId)).ReturnsAsync(rootOrganization);
+
+            var cancelationToken = new CancellationToken();
+
+            // Act & Assert
+            Func<Task> act = async () => await _createOrganizationHandler.HandleAsync(comand, cancelationToken);
+            await act.Should().ThrowAsync<MiniSpace.Services.Organizations.Application.Exceptions.UnauthorizedAccessException>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_InavalidOrganizationRoot_ShouldThrowRootOrganizationNotFoundException()
+        {
+            // Arrange
+            var rootOrganizationId = Guid.NewGuid();
+            var organizationId = Guid.NewGuid();
+            var parentId = Guid.NewGuid();
+            var comand = new CreateOrganization(organizationId, "this", rootOrganizationId, parentId);
+
+            var organization = new Organization(organizationId, "this");
+            var parent = new Organization(parentId, "parent", new List<Organizer>() { }, new List<Organization>() { });
+            var rootOrganization = new Organization(rootOrganizationId, "root", new List<Organizer>() { }, new List<Organization>() { });
+            var identityContext = new IdentityContext(Guid.NewGuid().ToString(), "Admin", true, new Dictionary<string, string>());
+
+            _appContextMock.Setup(ctx => ctx.Identity).Returns(identityContext);
+            _organizationRepositoryMock.Setup(repo => repo.GetAsync(comand.RootId)).ReturnsAsync((Organization)null);
+
+            var cancelationToken = new CancellationToken();
+
+            // Act & Assert
+            Func<Task> act = async () => await _createOrganizationHandler.HandleAsync(comand, cancelationToken);
+            await act.Should().ThrowAsync<RootOrganizationNotFoundException>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithoutOrganizationInOrganizationRoot_ShouldThrowParentOrganizationNotFoundException()
+        {
+            // Arrange
+            var rootOrganizationId = Guid.NewGuid();
+            var organizationId = Guid.NewGuid();
+            var parentId = Guid.NewGuid();
+            var comand = new CreateOrganization(organizationId, "this", rootOrganizationId, parentId);
+
+            var organization = new Organization(organizationId, "this");
+            var parent = new Organization(parentId, "parent", new List<Organizer>() { }, new List<Organization>() { });
+            var rootOrganization = new Organization(rootOrganizationId, "root", new List<Organizer>() { }, new List<Organization>() { });
+            var identityContext = new IdentityContext(Guid.NewGuid().ToString(), "Admin", true, new Dictionary<string, string>());
+
+            _appContextMock.Setup(ctx => ctx.Identity).Returns(identityContext);
+            _organizationRepositoryMock.Setup(repo => repo.GetAsync(comand.RootId)).ReturnsAsync(rootOrganization);
+
+            var cancelationToken = new CancellationToken();
+
+            // Act & Assert
+            Func<Task> act = async () => await _createOrganizationHandler.HandleAsync(comand, cancelationToken);
+            await act.Should().ThrowAsync<ParentOrganizationNotFoundException>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithInvalidName_ShouldThrowInvalidOrganizationNameException()
+        {
+            // Arrange
+            var rootOrganizationId = Guid.NewGuid();
+            var organizationId = Guid.NewGuid();
+            var parentId = Guid.NewGuid();
+            var comand = new CreateOrganization(organizationId, "", rootOrganizationId, parentId);
+
+            var organization = new Organization(organizationId, "this");
+            var parent = new Organization(parentId, "parent", new List<Organizer>() { }, new List<Organization>() { });
+            var rootOrganization = new Organization(rootOrganizationId, "root", new List<Organizer>() { }, new List<Organization>() { parent });
+            var identityContext = new IdentityContext(Guid.NewGuid().ToString(), "Admin", true, new Dictionary<string, string>());
+
+            _appContextMock.Setup(ctx => ctx.Identity).Returns(identityContext);
+            _organizationRepositoryMock.Setup(repo => repo.GetAsync(comand.RootId)).ReturnsAsync(rootOrganization);
+
+            var cancelationToken = new CancellationToken();
+
+            // Act & Assert
+            Func<Task> act = async () => await _createOrganizationHandler.HandleAsync(comand, cancelationToken);
+            await act.Should().ThrowAsync<InvalidOrganizationNameException>();
+        }
+    }
+}
---
+++ MiniSpace.Services.Organizations/tests/MiniSpace.Services.Organizations.Application.UnitTests/Commands/Handlers/CreateRoorOrganizationHandlerTest.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using MiniSpace.Services.Organizations.Application.Commands;
+using MiniSpace.Services.Organizations.Application.Commands.Handlers;
+using MiniSpace.Services.Organizations.Application.Exceptions;
+using MiniSpace.Services.Organizations.Application.Services;
+using MiniSpace.Services.Organizations.Core.Entities;
+using MiniSpace.Services.Organizations.Core.Repositories;
+using MiniSpace.Services.Organizations.Infrastructure.Contexts;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MiniSpace.Services.Organizations.Application.UnitTests.Commands.Handlers
+{
+    public class CreateRoorOrganizationHandlerTest
+    {
+        private readonly CreateRootOrganizationHandler _createRootOrganizationHandler;
+        private readonly Mock<IOrganizationRepository> _organizationRepositoryMock;
+        private readonly Mock<IAppContext> _appContextMock;
+        private readonly Mock<IMessageBroker> _messageBrokerMock;
+
+        public CreateRoorOrganizationHandlerTest()
+        {
+            _organizationRepositoryMock = new Mock<IOrganizationRepository>();
+            _appContextMock = new Mock<IAppContext>();
+            _messageBrokerMock = new Mock<IMessageBroker>();
+            _createRootOrganizationHandler = new CreateRootOrganizationHandler(
+                _organizationRepositoryMock.Object,
+                _appContextMock.Object,
+                _messageBrokerMock.Object
+                );
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithValidOrganizationNameAndAuthorised_ShouldNotThrow()
+        {
+            // Arrange
+            var organizationId = Guid.NewGuid();
+            var comand = new CreateRootOrganization(organizationId, "this");
+
+            var organization = new Organization(organizationId, "this");
+
+            var identityContext = new IdentityContext(Guid.NewGuid().ToString(), "Admin", true, new Dictionary<string, string>());
+
+            _appContextMock.Setup(ctx => ctx.Identity).Returns(identityContext);
+            
+            var cancelationToken = new CancellationToken();
+
+            // Act & Assert
+            Func<Task> act = async () => await _createRootOrganizationHandler.HandleAsync(comand, cancelationToken);
+            await act.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async Task HandleAsync_WithoutAdminRole_ShouldThrowUnauthorizedAccessException()
+        {
+            // Arrange
+            var organizationId = Guid.NewGuid();
+            var comand = new CreateRootOrganization(organizationId, "this");
+
+            var identityContext = new IdentityContext(Guid.NewGuid().ToString(), "", true, new Dictionary<string, string>());
+
+            _appContextMock.Setup(ctx => ctx.Identity).Returns(identityContext);
+
+            var cancelationToken = new CancellationToken();
+
+            // Act & Assert
+            Func<Task> act = async () => await _createRootOrganizationHandler.HandleAsync(comand, cancelationToken);
+            await act.Should().ThrowAsync<MiniSpace.Services.Organizations.Application.Exceptions.UnauthorizedAccessException>();
+        }
+
+        [Fact]
+        public async Task HandleAsync_InavalidOrganizationName_ShouldThrowInvalidOrganizationNameException()
+        {
+            // Arrange
+            var organizationId = Guid.NewGuid();
+            var comand = new CreateRootOrganization(organizationId, "");
+
+            var identityContext = new IdentityContext(Guid.NewGuid().ToString(), "Admin", true, new Dictionary<string, string>());
+
+            _appContextMock.Setup(ctx => ctx.Identity).Returns(identityContext);
+
+            var cancelationToken = new CancellationToken();
+
+            // Act & Assert
+            Func<Task> act = async () => await _createRootOrganizationHandler.HandleAsync(comand, cancelationToken);
+            await act.Should().ThrowAsync<InvalidOrganizationNameException> ();
+        }
+    }
+}
