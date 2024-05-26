fdd5024257269302d1ee417fc9ccda3b2c8d8cd3
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-05-27 01:21:09 +0200

    (#126) adding the basic entities for the email notifications

---
+++ MiniSpace.Services.Email/src/MiniSpace.Services.Notifications.Core/Entities/UserPreferences.cs
@@ -0,0 +1,14 @@
+namespace MiniSpace.Services.Notifications.Core.Entities
+{
+    public class UserPreferences
+    {
+        public Guid UserId { get; set; }
+        public bool ReceivesEmailNotifications { get; set; }
+
+        public UserPreferences(Guid userId, bool receivesEmailNotifications)
+        {
+            UserId = userId;
+            ReceivesEmailNotifications = receivesEmailNotifications;
+        }
+    }
+}
