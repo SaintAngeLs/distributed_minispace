5499d4ef973014d67c38f5507422e4792100c7bf
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-05-26 21:12:17 +0200

    (#125) adding the evnets dtop

--- MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Api/obj/Debug/net8.0/MiniSpace.Services.Notifications.Api.AssemblyInfo.cs
+++ MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Api/obj/Debug/net8.0/MiniSpace.Services.Notifications.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniSpace.Services.Notifications.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aed68beb8435c39687a13554ee82deae30cb9b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aecc9d05974bc43ed058510bd7830ea500369861")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniSpace.Services.Notifications.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniSpace.Services.Notifications.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
