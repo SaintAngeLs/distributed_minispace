d5de2ad487dd586ab7d8e17dcc2350b037ae8a58
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-05-27 01:22:02 +0200

    (#126) handling the student emails entities in the core entities

---
+++ MiniSpace.Services.Email/src/MiniSpace.Services.Notifications.Core/Entities/StudentEmails.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniSpace.Services.Notifications.Core.Entities
+{
+    public class StudentEmails
+    {
+        public Guid StudentId { get; private set; }
+        private List<EmailNotification> _emailNotifications;
+
+        public IEnumerable<EmailNotification> EmailNotifications => _emailNotifications.AsReadOnly();
+
+        public StudentEmails(Guid studentId)
+        {
+            StudentId = studentId;
+            _emailNotifications = new List<EmailNotification>();
+        }
+
+        public void AddEmailNotification(EmailNotification emailNotification)
+        {
+            if (emailNotification != null)
+            {
+                _emailNotifications.Add(emailNotification);
+            }
+        }
+
+        public void RemoveEmailNotification(Guid emailNotificationId)
+        {
+            _emailNotifications.RemoveAll(e => e.EmailNotificationId == emailNotificationId);
+        }
+
+        public void MarkEmailAsSent(Guid emailNotificationId)
+        {
+            var emailNotification = _emailNotifications.FirstOrDefault(e => e.EmailNotificationId == emailNotificationId);
+            if (emailNotification != null)
+            {
+                emailNotification.MarkAsSent();
+            }
+        }
+    }
+}
