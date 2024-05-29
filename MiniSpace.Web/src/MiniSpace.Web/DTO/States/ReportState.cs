32be0c7a71a1dcb74f6987b84ab09ea5bd0d4275
Amadeusz Nowak <an2508374@gmail.com>
2024-05-29 21:18:42 +0200

    (#57) enhance showed values of context type, category and state

--- MiniSpace.Web/src/MiniSpace.Web/DTO/States/ReportState.cs
+++ MiniSpace.Web/src/MiniSpace.Web/DTO/States/ReportState.cs
@@ -1,4 +1,6 @@
-﻿namespace MiniSpace.Web.DTO.States
+﻿using System;
+
+namespace MiniSpace.Web.DTO.States
 {
     public enum ReportState
     {
@@ -7,5 +9,21 @@
         Resolved,
         Rejected,
         Cancelled
+    }
+    
+    public static class ReportStateExtensions
+    {
+        public static string GetReportStateText(ReportState reportState)
+        {
+            return reportState switch
+            {
+                ReportState.Submitted => "Submitted",
+                ReportState.UnderReview => "Under review",
+                ReportState.Resolved => "Resolved",
+                ReportState.Rejected => "Rejected",
+                ReportState.Cancelled => "Cancelled",
+                _ => throw new ArgumentOutOfRangeException(nameof(reportState), reportState, null)
+            };
+        }
     }
 }
