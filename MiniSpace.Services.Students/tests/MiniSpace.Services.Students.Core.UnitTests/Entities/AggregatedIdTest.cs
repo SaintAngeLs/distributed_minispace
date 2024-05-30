0fefcbb4b5faf77f8f88dce5e2acb146226e56b2
OJ <Zniwiarz@xd.xd>
2024-05-30 16:28:25 +0200

    (#187) Core: Entities tests

---
+++ MiniSpace.Services.Students/tests/MiniSpace.Services.Students.Core.UnitTests/Entities/AggregatedIdTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using MiniSpace.Services.Students.Application.Events.External;
+using MiniSpace.Services.Students.Core.Entities;
+using MiniSpace.Services.Students.Core.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MiniSpace.Services.Students.Core.UnitTests.Entities
+{
+    public class AggregatedIdTest
+    {
+        [Fact]
+        public void AggregateId_EmptyValue_ShouldThrowInvalidAggregateIdException()
+        {
+            // Assert & Arrange & Act
+            Func<AggregateId> func = () => {return new AggregateId(Guid.Empty); };
+            func.Should().Throw<InvalidAggregateIdException>();
+        }
+        [Fact]
+        public void AggregateId_CreatedTwice_ShuldBeDiffrent()
+        {
+            // Arrange & Act
+            var id1 = new AggregateId();
+            var id2 = new AggregateId();
+
+            // Assert
+            Assert.NotEqual(id1.Value, id2.Value);
+        }
+
+        [Fact]
+        public void AggregateId_CreatedTwiceSameGuid_ShuldBeSame()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var id1 = new AggregateId(id);
+            var id2 = new AggregateId(id);
+
+            // Assert
+            Assert.Equal(id1.Value, id2.Value);
+        }
+    }
+}
---
+++ MiniSpace.Services.Students/tests/MiniSpace.Services.Students.Core.UnitTests/Entities/StudentTest.cs
@@ -0,0 +1,172 @@
+﻿using FluentAssertions;
+using MiniSpace.Services.Students.Application.Exceptions;
+using MiniSpace.Services.Students.Core.Entities;
+using MiniSpace.Services.Students.Core.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MiniSpace.Services.Students.Core.UnitTests.Entities
+{
+    public class StudentTest
+    {
+        [Fact]
+        public void Student_WrongName_ShouldThrowInvalidStudentFullNameException()
+        {
+            //Act & Assert
+            Func<Student> fun = () => { return new Student(Guid.NewGuid(), "", "", "an@email.com", DateTime.Now);  };
+            Assert.Throws<InvalidStudentFullNameException>(() => fun());
+        }
+
+        [Fact]
+        public void CompleteRegistration_StudentIncomleated_ShouldMakeStudentValid()
+        {
+            // Arrange
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+            var description = "desc";
+            var dOB = new DateTime(2000, 1, 1);
+
+            // Act
+            student.CompleteRegistration(Guid.NewGuid(), description, dOB, DateTime.Now, true);
+
+            // Assert
+            Assert.Equal(State.Valid, student.State);
+        }
+
+        [Fact]
+        public void CompleteRegistration_StudentAlreadyCompleated_ShouldThrowCannotChangeStudentStateException()
+        {
+            // Arrange
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+            var description = "desc";
+            var dOB = new DateTime(2000, 1, 1);
+            student.SetValid();
+
+            // Act
+            Func<bool> fun = () => { student.CompleteRegistration(Guid.NewGuid(), description, dOB, DateTime.Now, true); return true; };
+
+            // Assert
+            Assert.Throws<CannotChangeStudentStateException>(() => fun());
+        }
+
+        [Fact]
+        public void CompleteRegistration_DescriptionNullorWhite_ShouldThrowInvalidStudentDescriptionException()
+        {
+            // Arrange
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+            var description = "";
+            var dOB = new DateTime(2000, 1, 1);
+
+            // Act
+            Func<bool> fun = () => { student.CompleteRegistration(Guid.NewGuid(), description, dOB, DateTime.Now, true); return true; };
+
+            // Assert
+            Assert.Throws<InvalidStudentDescriptionException>(() => fun());
+        }
+
+        [Fact]
+        public void CompleteRegistration_DateOfBirthWrong_ShouldThrowInvalidStudentDateOfBirthException()
+        {
+            // Arrange
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+            var description = "text";
+            var dOB = new DateTime(3000, 1, 1);
+
+            // Act
+            Func<bool> fun = () => { student.CompleteRegistration(Guid.NewGuid(), description, dOB, DateTime.Now, true); return true; };
+
+            // Assert
+            Assert.Throws<InvalidStudentDateOfBirthException>(() => fun());
+        }
+
+        [Fact]
+        public void Update_StudentIsNotVaild_ShouldThrowCannotUpdateStudentException()
+        {
+            // Arrange
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+            var description = "text";
+
+            // Act
+            Func<bool> fun = () => { student.Update(Guid.NewGuid(), description, true); return true; };
+
+            // Assert
+            Assert.Throws<CannotUpdateStudentException>(() => fun());
+        }
+
+        [Fact]
+        public async void Update_DescriptionNullorWhite_ShouldThrowInvalidStudentDescriptionException()
+        {
+            // Arrange
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+            var description = "";
+            student.SetValid();
+
+            // Act
+            Func<bool> fun = () => { student.Update(Guid.NewGuid(), description, true); return true; };
+
+            // Assert
+            Assert.Throws<InvalidStudentDescriptionException>(() => fun());
+        }
+
+        [Fact]
+        public async void AddInterestedInEvent_AlreadyAdded_ShouldThrowEventAlreadyAddedExeption()
+        {
+            // Arrange
+            var eventId = Guid.NewGuid();
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+            student.InterestedInEvents.Append(eventId);
+
+            // Act
+            Func<bool> fun = () => { student.AddInterestedInEvent(eventId); return true; };
+
+            // Assert
+            Assert.ThrowsAny<DomainException>(() => fun());
+        }
+
+        [Fact]
+        public async void RemoveInterestedInEvent_AlreadyRemoved_ShouldThrowEventAlreadyRemovedExeption()
+        {
+            // Arrange
+            var eventId = Guid.NewGuid();
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+
+            // Act
+            Func<bool> fun = () => { student.RemoveInterestedInEvent(eventId); return true; };
+
+            // Assert
+            Assert.ThrowsAny<DomainException>(() => fun());
+        }
+
+        [Fact]
+        public async void AddISignedUpEvent_AlreadyAdded_ShouldThrowEventAlreadyAddedExeption()
+        {
+            // Arrange
+            var eventId = Guid.NewGuid();
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+            student.SignedUpEvents.Append(eventId);
+
+            // Act
+            Func<bool> fun = () => { student.AddInterestedInEvent(eventId); return true; };
+
+            // Assert
+            Assert.ThrowsAny<DomainException>(() => fun());
+        }
+
+        [Fact]
+        public async void RemoveSignedUpEvent_AlreadyRemoved_ShouldThrowEventAlreadyRemovedExeption()
+        {
+            // Arrange
+            var eventId = Guid.NewGuid();
+            var student = new Student(Guid.NewGuid(), "Adam", "Nowak", "an@email.com", DateTime.Now);
+
+            // Act
+            Func<bool> fun = () => { student.RemoveInterestedInEvent(eventId); return true; };
+
+            // Assert
+            Assert.ThrowsAny<DomainException>(() => fun());
+        }
+    }
+}
