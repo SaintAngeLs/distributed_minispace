41ed5207fbc622fc016c9430336bd5988b16488a
Amadeusz Nowak <an2508374@gmail.com>
2024-05-31 22:35:23 +0200

    (#235) add refreshing organization tree after deleting an organization

--- MiniSpace.Web/src/MiniSpace.Web/Models/Organizations/OrganizationModel.cs
+++ MiniSpace.Web/src/MiniSpace.Web/Models/Organizations/OrganizationModel.cs
@@ -8,6 +8,7 @@ namespace MiniSpace.Web.Models.Organizations
         public Guid Id { get; set; }
         public string Name { get; set; }
         public Guid RootId { get; set; }
+        public OrganizationModel Parent { get; set; }
         public List<OrganizationModel> Children { get; set; }
     }
 }
