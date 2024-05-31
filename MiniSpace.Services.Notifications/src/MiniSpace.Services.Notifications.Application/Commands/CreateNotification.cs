36ce51e2a005fdb20b6aa25d82d2da1d96783941
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-05-31 09:37:19 +0200

    (#125) (#126) (#229) update create notification command handler

--- MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Commands/CreateNotification.cs
+++ MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Commands/CreateNotification.cs
@@ -7,12 +7,15 @@ namespace MiniSpace.Services.Notifications.Application.Commands
         public Guid NotificationId { get; }
         public Guid UserId { get; }
         public string Message { get; }
-
-        public CreateNotification(Guid notificationId, Guid userId, string message)
+        public IEnumerable<Guid> StudentIds { get; }
+        public Guid? EventId { get; }
+        public CreateNotification(Guid notificationId, Guid userId, string message, IEnumerable<Guid> studentIds, Guid? eventId = null)
         {
             NotificationId = notificationId;
             UserId = userId;
             Message = message;
+            StudentIds = studentIds;
+            EventId = eventId;
         }
     }
 }
--- MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Commands/Handlers/CreateNotificationHandler.cs
+++ MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Commands/Handlers/CreateNotificationHandler.cs
@@ -2,18 +2,29 @@ using Convey.CQRS.Commands;
 using MiniSpace.Services.Notifications.Core.Repositories;
 using MiniSpace.Services.Notifications.Core.Entities;
 using MiniSpace.Services.Notifications.Application.Services;
+using MiniSpace.Services.Notifications.Application.Services.Clients;
 
 namespace MiniSpace.Services.Notifications.Application.Commands.Handlers
 {
     public class CreateNotificationHandler : ICommandHandler<CreateNotification>
     {
-        private readonly INotificationRepository _notificationRepository;
+        private readonly IStudentNotificationsRepository _studentNotificationsRepository;
+        private readonly IFriendsServiceClient _friendsServiceClient;
+        private readonly IEventsServiceClient _eventsServiceClient;
         private readonly IEventMapper _eventMapper;
         private readonly IMessageBroker _messageBroker;
 
-        public CreateNotificationHandler(INotificationRepository notificationRepository, IEventMapper eventMapper, IMessageBroker messageBroker)
+        public CreateNotificationHandler(
+            IStudentNotificationsRepository studentNotificationsRepository,
+            IFriendsServiceClient friendsServiceClient,
+             IEventsServiceClient eventsServiceClient,
+            IEventMapper eventMapper, 
+            IMessageBroker messageBroker
+            )
         {
-            _notificationRepository = notificationRepository;
+            _studentNotificationsRepository = studentNotificationsRepository;
+            _friendsServiceClient = friendsServiceClient;
+             _eventsServiceClient = eventsServiceClient;
             _eventMapper = eventMapper;
             _messageBroker = messageBroker;
         }
