08ff34f0116d2deb819d2c2542078b2fa8c493f5
OJ <Zniwiarz@xd.xd>
2024-06-02 10:20:57 +0200

    (#148) Infractructure: missed exclusion

--- MiniSpace.Services.Comments/src/MiniSpace.Services.Comments.Infrastructure/Services/DateTimeProvider.cs
+++ MiniSpace.Services.Comments/src/MiniSpace.Services.Comments.Infrastructure/Services/DateTimeProvider.cs
@@ -1,7 +1,9 @@
 using MiniSpace.Services.Comments.Application.Services;
+using System.Diagnostics.CodeAnalysis;
 
 namespace MiniSpace.Services.Comments.Infrastructure.Services
 {
+    [ExcludeFromCodeCoverage]
     internal sealed class DateTimeProvider : IDateTimeProvider
     {
         public DateTime Now  => DateTime.UtcNow;
