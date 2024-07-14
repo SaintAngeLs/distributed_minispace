83798493eb2b4e268a9e940703e9dca8035a9f18
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-07-14 04:54:54 +0200

    #289 update the new frineds search full razor page

--- MiniSpace.Web/src/MiniSpace.Web/Startup.cs
+++ MiniSpace.Web/src/MiniSpace.Web/Startup.cs
@@ -118,7 +118,6 @@ namespace MiniSpace.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
