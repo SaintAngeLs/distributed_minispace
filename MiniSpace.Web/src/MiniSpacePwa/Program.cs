a6e0bed8f10582c56aeb7c89c693e82ebe015e6e
Amadeusz Nowak <an2508374@gmail.com>
2024-07-17 22:15:34 +0200

    (#116)(#291) add MudBlazor to wasm

--- MiniSpace.Web/src/MiniSpacePwa/Program.cs
+++ MiniSpace.Web/src/MiniSpacePwa/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MiniSpacePwa;
 using MiniSpacePwa.HttpClients;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,5 +17,8 @@ builder.Services.AddHttpClient<IHttpClient, CustomHttpClient>((serviceProvider, 
     var options = serviceProvider.GetRequiredService<HttpClientOptions>();
     client.BaseAddress = new Uri(options.ApiUrl);
 });
+
+// Add MudServices
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
