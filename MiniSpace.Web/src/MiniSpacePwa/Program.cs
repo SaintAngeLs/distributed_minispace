29c5ae4cc8dd60649f5904581a1b8523bb94dcc4
Amadeusz Nowak <an2508374@gmail.com>
2024-07-17 22:39:58 +0200

    (#116)(#291) add basic pages to wasm

--- MiniSpace.Web/src/MiniSpacePwa/Program.cs
+++ MiniSpace.Web/src/MiniSpacePwa/Program.cs
@@ -1,6 +1,20 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MiniSpacePwa;
+using MiniSpacePwa.Areas.Comments;
+using MiniSpacePwa.Areas.Events;
+using MiniSpacePwa.Areas.Friends;
+using MiniSpacePwa.Areas.Http;
+using MiniSpacePwa.Areas.Identity;
+using MiniSpacePwa.Areas.MediaFiles;
+using MiniSpacePwa.Areas.Notifications;
+using MiniSpacePwa.Areas.Organizations;
+using MiniSpacePwa.Areas.Posts;
+using MiniSpacePwa.Areas.Reactions;
+using MiniSpacePwa.Areas.Reports;
+using MiniSpacePwa.Areas.Students;
 using MiniSpacePwa.HttpClients;
 using MudBlazor.Services;
 
@@ -17,6 +31,26 @@ builder.Services.AddHttpClient<IHttpClient, CustomHttpClient>((serviceProvider, 
     var options = serviceProvider.GetRequiredService<HttpClientOptions>();
     client.BaseAddress = new Uri(options.ApiUrl);
 });
+
+// Add local storage and other services
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<Radzen.DialogService, Radzen.DialogService>();
+builder.Services.AddScoped<Radzen.DialogService, Radzen.DialogService>();
+builder.Services.AddScoped<Radzen.NotificationService>(); 
+builder.Services.AddScoped<IIdentityService, IdentityService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<IStudentsService, StudentsService>();
+builder.Services.AddScoped<IEventsService, EventsService>();
+builder.Services.AddScoped<IPostsService, PostsService>();
+builder.Services.AddScoped<IMediaFilesService, MediaFilesService>();
+builder.Services.AddScoped<IOrganizationsService, OrganizationsService>();
+builder.Services.AddScoped<IErrorMapperService, ErrorMapperService>();
+builder.Services.AddScoped<IFriendsService, FriendsService>();
+builder.Services.AddScoped<INotificationsService, NotificationsService>();
+builder.Services.AddScoped<SignalRService>();
+builder.Services.AddScoped<IReactionsService, ReactionsService>();
+builder.Services.AddScoped<ICommentsService, CommentsService>();
+builder.Services.AddScoped<IReportsService, ReportsService>();
 
 // Add MudServices
 builder.Services.AddMudServices();
