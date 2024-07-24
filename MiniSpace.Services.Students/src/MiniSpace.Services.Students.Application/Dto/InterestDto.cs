15784afaa1e3309289c4ff9f188489196181adbc
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-07-24 02:59:44 +0200

    (#329) users' dts related update

---
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Dto/InterestDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MiniSpace.Services.Students.Application.Dto
+{
+    [ExcludeFromCodeCoverage]
+    public class InterestDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Dto/UserSettingsDto.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MiniSpace.Services.Students.Application.Dto
+{
+    [ExcludeFromCodeCoverage]
+    public class UserSettingsDto
+    {
+        public string CreatedAtVisibility { get; set; }
+        public string DateOfBirthVisibility { get; set; }
+        public string InterestedInEventsVisibility { get; set; }
+        public string SignedUpEventsVisibility { get; set; }
+        public string EducationVisibility { get; set; }
+        public string WorkPositionVisibility { get; set; }
+        public string LanguagesVisibility { get; set; }
+        public string InterestsVisibility { get; set; }
+        public string ContactEmailVisibility { get; set; }
+        public string PhoneNumberVisibility { get; set; }
+    }
+}
