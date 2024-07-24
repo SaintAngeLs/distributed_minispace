2ca0d0b6817209036c1a0e261ec461601bb15413
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-07-24 04:01:31 +0200

    (#329) update media file upload, medi file delete handler

--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Events/External/Handlers/MediaFileDeletedHandler.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Events/External/Handlers/MediaFileDeletedHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Convey.CQRS.Events;
 using MiniSpace.Services.Students.Core.Repositories;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,21 +10,17 @@ namespace MiniSpace.Services.Students.Application.Events.External.Handlers
     public class MediaFileDeletedHandler : IEventHandler<MediaFileDeleted>
     {
         private readonly IStudentRepository _studentRepository;
+        private readonly IUserGalleryRepository _userGalleryRepository;
 
-        public MediaFileDeletedHandler(IStudentRepository studentRepository)
+        public MediaFileDeletedHandler(IStudentRepository studentRepository, IUserGalleryRepository userGalleryRepository)
         {
             _studentRepository = studentRepository;
+            _userGalleryRepository = userGalleryRepository;
         }
 
         public async Task HandleAsync(MediaFileDeleted @event, CancellationToken cancellationToken)
         {
             Console.WriteLine($"Received MediaFileDeleted event: {@event.MediaFileUrl}");
-
-            if (@event.Source.ToLowerInvariant() != "studentprofileimage")
-            {
-                Console.WriteLine("Event source is not 'studentprofileimage', ignoring the event.");
-                return;
-            }
 
             var student = await _studentRepository.GetAsync(@event.SourceId);
             if (student != null)
@@ -31,7 +28,7 @@ namespace MiniSpace.Services.Students.Application.Events.External.Handlers
                 bool updated = false;
 
                 // Check and remove profile image
-                if (student.ProfileImageUrl == @event.MediaFileUrl)
+                if (@event.Source.ToLowerInvariant() == "studentprofileimage" && student.ProfileImageUrl == @event.MediaFileUrl)
                 {
                     student.RemoveProfileImage();
                     updated = true;
@@ -39,19 +36,11 @@ namespace MiniSpace.Services.Students.Application.Events.External.Handlers
                 }
 
                 // Check and remove banner image
-                if (student.BannerUrl == @event.MediaFileUrl)
+                if (@event.Source.ToLowerInvariant() == "studentbannerimage" && student.BannerUrl == @event.MediaFileUrl)
                 {
                     student.RemoveBannerImage();
                     updated = true;
                     Console.WriteLine("Removed banner image.");
-                }
-
-                // Check and remove gallery images
-                if (student.GalleryOfImageUrls.Contains(@event.MediaFileUrl))
-                {
-                    student.RemoveGalleryImage(@event.MediaFileUrl);
-                    updated = true;
-                    Console.WriteLine("Removed gallery image.");
                 }
 
                 if (updated)
@@ -60,7 +49,19 @@ namespace MiniSpace.Services.Students.Application.Events.External.Handlers
                     Console.WriteLine("Updated student repository.");
                 }
             }
-        }
 
+            // Check and remove gallery images
+            var userGallery = await _userGalleryRepository.GetAsync(@event.SourceId);
+            if (userGallery != null)
+            {
+                var galleryImage = userGallery.GalleryOfImages.FirstOrDefault(img => img.ImageUrl == @event.MediaFileUrl);
+                if (galleryImage != null)
+                {
+                    userGallery.RemoveGalleryImage(galleryImage.ImageId);
+                    await _userGalleryRepository.UpdateAsync(userGallery);
+                    Console.WriteLine("Removed gallery image.");
+                }
+            }
+        }
     }
 }
