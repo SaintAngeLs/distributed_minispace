46004b0c3dab919af1d6d30b598c4b08c98e3dad
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-07-29 00:29:55 +0200

    (#330) update organization members repository

--- MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Core/Repositories/IOrganizationMembersRepository.cs
+++ MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Core/Repositories/IOrganizationMembersRepository.cs
@@ -1,5 +1,6 @@
 using MiniSpace.Services.Organizations.Core.Entities;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MiniSpace.Services.Organizations.Core.Repositories
@@ -8,8 +9,8 @@ namespace MiniSpace.Services.Organizations.Core.Repositories
     {
         Task<User> GetMemberAsync(Guid organizationId, Guid memberId);
         Task<IEnumerable<User>> GetMembersAsync(Guid organizationId);
-        Task AddMemberAsync(User member);
-        Task UpdateMemberAsync(User member);
+        Task AddMemberAsync(Guid organizationId, User member);
+        Task UpdateMemberAsync(Guid organizationId, User member);
         Task DeleteMemberAsync(Guid organizationId, Guid memberId);
     }
 }
