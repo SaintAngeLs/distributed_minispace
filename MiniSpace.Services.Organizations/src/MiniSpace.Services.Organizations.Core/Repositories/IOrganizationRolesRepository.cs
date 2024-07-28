477f8950b8627c5773bd58501514d0f53c3f3d00
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-07-28 21:18:35 +0200

    (#330) udpate core organization roles interface

--- MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Core/Repositories/IOrganizationRolesRepository.cs
+++ MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Core/Repositories/IOrganizationRolesRepository.cs
@@ -9,6 +9,7 @@ namespace MiniSpace.Services.Organizations.Core.Repositories
     {
         Task<Role> GetRoleAsync(Guid organizationId, Guid roleId);
         Task<IEnumerable<Role>> GetRolesAsync(Guid organizationId);
+        Task<Role> GetRoleByNameAsync(string roleName);
         Task AddRoleAsync(Role role);
         Task UpdateRoleAsync(Role role);
         Task DeleteRoleAsync(Guid roleId);
