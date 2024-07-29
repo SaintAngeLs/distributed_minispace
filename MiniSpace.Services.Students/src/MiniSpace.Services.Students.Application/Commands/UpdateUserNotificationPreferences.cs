b2c94c8828251d0f6a16e4e8059bdb1e34336d16
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-07-30 00:14:00 +0200

    (#330) udpate styudnet update command

--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Commands/UpdateUserNotificationPreferences.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Commands/UpdateUserNotificationPreferences.cs
@@ -5,30 +5,16 @@ namespace MiniSpace.Services.Students.Application.Commands
 {
     public class UpdateUserNotificationPreferences : ICommand
     {
-        public Guid StudentId { get; }
-        public bool AccountChanges { get; }
-        public bool SystemLogin { get; }
-        public bool NewEvent { get; }
-        public bool InterestBasedEvents { get; }
-        public bool EventNotifications { get; }
-        public bool CommentsNotifications { get; }
-        public bool PostsNotifications { get; }
-        public bool FriendsNotifications { get; }
-
-        public UpdateUserNotificationPreferences(Guid studentId, bool accountChanges, bool systemLogin, bool newEvent,
-                                                 bool interestBasedEvents, bool eventNotifications,
-                                                 bool commentsNotifications, bool postsNotifications,
-                                                 bool friendsNotifications)
-        {
-            StudentId = studentId;
-            AccountChanges = accountChanges;
-            SystemLogin = systemLogin;
-            NewEvent = newEvent;
-            InterestBasedEvents = interestBasedEvents;
-            EventNotifications = eventNotifications;
-            CommentsNotifications = commentsNotifications;
-            PostsNotifications = postsNotifications;
-            FriendsNotifications = friendsNotifications;
-        }
+        public Guid StudentId { get; set; }
+        public bool EmailNotifications { get; set; }
+        public bool AccountChanges { get; set; }
+        public bool SystemLogin { get; set; }
+        public bool NewEvent { get; set; }
+        public bool InterestBasedEvents { get; set; }
+        public bool EventNotifications { get; set; }
+        public bool CommentsNotifications { get; set; }
+        public bool PostsNotifications { get; set; }
+        public bool FriendsNotifications { get; set; }
     }
+
 }
