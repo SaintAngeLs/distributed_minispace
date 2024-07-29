c12a9fa800452135f12c9dceb49f4eaedad68bb5
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-07-29 19:02:55 +0200

    (#330) update application events for organisation images upload

--- MiniSpace.Services.MediaFiles/src/MiniSpace.Services.MediaFiles.Application/Events/OrganizationImageUploaded.cs
+++ MiniSpace.Services.MediaFiles/src/MiniSpace.Services.MediaFiles.Application/Events/OrganizationImageUploaded.cs
@@ -3,16 +3,16 @@ using System;
 
 namespace MiniSpace.Services.MediaFiles.Application.Events
 {
-    public class StudentImageUploaded : IEvent
+    public class OrganizationImageUploaded : IEvent
     {
-        public Guid StudentId { get; }
+        public Guid OrganizationId { get; }
         public string ImageUrl { get; }
         public string ImageType { get; }
         public DateTime UploadDate { get; }
 
-        public StudentImageUploaded(Guid studentId, string imageUrl, string imageType, DateTime uploadDate)
+        public OrganizationImageUploaded(Guid organizationId, string imageUrl, string imageType, DateTime uploadDate)
         {
-            StudentId = studentId;
+            OrganizationId = organizationId;
             ImageUrl = imageUrl;
             ImageType = imageType;
             UploadDate = uploadDate;
