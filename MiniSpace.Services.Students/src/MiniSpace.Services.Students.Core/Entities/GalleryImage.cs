826e3960d131057d7d729e999443f1dc56be120a
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-07-31 14:21:20 +0200

    (#330) update gallery iamge core class

--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Core/Entities/GalleryImage.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Core/Entities/GalleryImage.cs
@@ -7,12 +7,14 @@ namespace MiniSpace.Services.Students.Core.Entities
         public Guid ImageId { get; private set; }
         public string ImageUrl { get; private set; }
         public DateTime DateAdded { get; private set; }
+        public string Description { get; private set; } 
 
-        public GalleryImage(Guid imageId, string imageUrl, DateTime dateAdded)
+        public GalleryImage(Guid imageId, string imageUrl, DateTime dateAdded, string description)
         {
             ImageId = imageId;
             ImageUrl = imageUrl ?? throw new ArgumentNullException(nameof(imageUrl));
             DateAdded = dateAdded;
+            Description = description; 
         }
     }
 }
