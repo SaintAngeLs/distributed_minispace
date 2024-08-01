8459c723a9e42f027ab32bdfe196a4b0cd7c84e3
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-01 17:25:58 +0200

    #330 update mediafiles service evens for mediafile deleated

--- MiniSpace.Services.MediaFiles/src/MiniSpace.Services.MediaFiles.Application/Events/MediaFileDeleted.cs
+++ MiniSpace.Services.MediaFiles/src/MiniSpace.Services.MediaFiles.Application/Events/MediaFileDeleted.cs
@@ -7,12 +7,14 @@ namespace MiniSpace.Services.MediaFiles.Application.Events
         public string MediaFileUrl { get; }
         public Guid SourceId { get; }
         public string Source { get; }
+        public Guid UploaderId { get; }
 
-        public MediaFileDeleted(string mediaFileUrl, Guid sourceId, string source)
+        public MediaFileDeleted(string mediaFileUrl, Guid sourceId, string source, Guid uploaderId)
         {
             MediaFileUrl = mediaFileUrl;
             SourceId = sourceId;
             Source = source;
+            UploaderId = uploaderId;
         }
     }
 }
