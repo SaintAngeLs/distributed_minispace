ccd6c65bfc0dacebdae47919405180caaff0dddc
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-01 22:49:19 +0200

    (#330) udpate organisation create view

--- MiniSpace.Web/src/MiniSpace.Web/Areas/Organizations/CommandsDto/CreateOrganizationDto.cs
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/Organizations/CommandsDto/CreateOrganizationDto.cs
@@ -8,8 +8,8 @@ namespace MiniSpace.Web.Areas.Organizations.CommandsDto
         public Guid OrganizationId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public Guid RootId { get; set; }
-        public Guid ParentId { get; set; }
+        public Guid? RootId { get; set; } 
+        public Guid? ParentId { get; set; } = null; 
         public Guid OwnerId { get; set; }
         public OrganizationSettingsDto Settings { get; set; }
         public string BannerUrl { get; set; }
