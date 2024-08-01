48e30c5901e79a6f893929d9e900f4f684586005
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-01 18:44:03 +0200

    (#330) remove the curved and redundand radzen compoments for organisations

--- MiniSpace.Web/src/MiniSpace.Web/DTO/Organizations/OrganizationDto.cs
+++ MiniSpace.Web/src/MiniSpace.Web/DTO/Organizations/OrganizationDto.cs
@@ -6,6 +6,9 @@ namespace MiniSpace.Web.DTO.Organizations
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public Guid RootId { get; set; }
+        public string Description { get; set; }
+        public string BannerUrl { get; set; }
+        public string ImageUrl { get; set; }
+        public Guid OwnerId { get; set; }
     }    
 }
