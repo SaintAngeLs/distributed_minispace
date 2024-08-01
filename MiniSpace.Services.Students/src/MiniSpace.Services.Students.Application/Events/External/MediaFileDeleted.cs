6b4d9158eb36f279d8d97c1aea5df17a93858f7f
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-01 17:28:22 +0200

    (#330) medifile deleted external evnet update, MediaFileDeleted evnetHandler update

--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Events/External/MediaFileDeleted.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Events/External/MediaFileDeleted.cs
@@ -1,5 +1,6 @@
 ﻿using Convey.CQRS.Events;
 using Convey.MessageBrokers;
+using System;
 
 namespace MiniSpace.Services.Students.Application.Events.External
 {
@@ -9,12 +10,14 @@ namespace MiniSpace.Services.Students.Application.Events.External
         public string MediaFileUrl { get; }
         public Guid SourceId { get; }
         public string Source { get; }
+        public Guid UploaderId { get; }
 
-        public MediaFileDeleted(string mediaFileUrl, Guid sourceId, string source)
+        public MediaFileDeleted(string mediaFileUrl, Guid sourceId, string source, Guid uploaderId)
         {
             MediaFileUrl = mediaFileUrl;
             SourceId = sourceId;
             Source = source;
+            UploaderId = uploaderId;
         }
     }
 }
