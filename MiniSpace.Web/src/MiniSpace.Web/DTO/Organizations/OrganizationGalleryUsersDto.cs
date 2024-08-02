04387e3d095734a7c6ef39516264fa532ef41c38
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-02 16:23:31 +0200

    (#330) udpate organizationDetailsDto

--- MiniSpace.Web/src/MiniSpace.Web/DTO/Organizations/OrganizationGalleryUsersDto.cs
+++ MiniSpace.Web/src/MiniSpace.Web/DTO/Organizations/OrganizationGalleryUsersDto.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 
+using System.Collections.Generic;
+
 namespace MiniSpace.Web.DTO.Organizations
 {
     public class OrganizationGalleryUsersDto
     {
-        public OrganizationDetailsDto Organization { get; set; }
+        public OrganizationDetailsDto OrganizationDetails { get; set; }
         public IEnumerable<GalleryImageDto> Gallery { get; set; }
         public IEnumerable<UserDto> Users { get; set; }
     }
