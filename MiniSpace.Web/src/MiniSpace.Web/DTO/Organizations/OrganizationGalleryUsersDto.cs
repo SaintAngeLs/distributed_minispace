0bffaeb3ccda0a8ebfc4f224367b5b5ac285b4f6
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-02 16:17:58 +0200

    (#330) add organization pages for administrator and user

--- MiniSpace.Web/src/MiniSpace.Web/DTO/Organizations/OrganizationGalleryUsersDto.cs
+++ MiniSpace.Web/src/MiniSpace.Web/DTO/Organizations/OrganizationGalleryUsersDto.cs
@@ -4,7 +4,7 @@ namespace MiniSpace.Web.DTO.Organizations
 {
     public class OrganizationGalleryUsersDto
     {
-        public OrganizationDto Organization { get; set; }
+        public OrganizationDetailsDto Organization { get; set; }
         public IEnumerable<GalleryImageDto> Gallery { get; set; }
         public IEnumerable<UserDto> Users { get; set; }
     }
