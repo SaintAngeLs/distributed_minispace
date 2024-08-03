64b9fff54e2b36bfad8b58ca4b369210d9084d36
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-03 11:50:37 +0200

    (#342) add nullable organization id to uploade media file command

--- MiniSpace.Services.MediaFiles/src/MiniSpace.Services.MediaFiles.Application/Commands/UploadMediaFile.cs
+++ MiniSpace.Services.MediaFiles/src/MiniSpace.Services.MediaFiles.Application/Commands/UploadMediaFile.cs
@@ -8,17 +8,19 @@ namespace MiniSpace.Services.MediaFiles.Application.Commands
         public Guid MediaFileId { get; set; }
         public Guid SourceId { get; set; }
         public string SourceType { get; set; }
+        public Guid? OrganizationId { get; set; }
         public Guid UploaderId { get; set; }
         public string FileName { get; set; }
         public string FileContentType { get; set; }
-        public byte[] FileData { get; set; } 
+        public byte[] FileData { get; set; }
 
-        public UploadMediaFile(Guid mediaFileId, Guid sourceId, string sourceType, Guid uploaderId,
-            string fileName, string fileContentType, byte[] fileData)
+        public UploadMediaFile(Guid mediaFileId, Guid sourceId, string sourceType, Guid? organizationId,
+            Guid uploaderId, string fileName, string fileContentType, byte[] fileData)
         {
             MediaFileId = mediaFileId == Guid.Empty ? Guid.NewGuid() : mediaFileId;
             SourceId = sourceId;
             SourceType = sourceType;
+            OrganizationId = organizationId; 
             UploaderId = uploaderId;
             FileName = fileName;
             FileContentType = fileContentType;
