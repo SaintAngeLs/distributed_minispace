3f10bf047e3f695a383f548680487ef848b4df36
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-03 03:09:41 +0200

    (#342) add external event organization image uploaded

---
+++ MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Application/Events/External/OrganizationImageUploaded.cs
@@ -0,0 +1,21 @@
+using Convey.CQRS.Events;
+using System;
+
+namespace MiniSpace.Services.Organizations.Application.Events.External
+{
+    public class OrganizationImageUploaded : IEvent
+    {
+        public Guid OrganizationId { get; }
+        public string ImageUrl { get; }
+        public string ImageType { get; }
+        public DateTime UploadDate { get; }
+
+        public OrganizationImageUploaded(Guid organizationId, string imageUrl, string imageType, DateTime uploadDate)
+        {
+            OrganizationId = organizationId;
+            ImageUrl = imageUrl;
+            ImageType = imageType;
+            UploadDate = uploadDate;
+        }
+    }
+}
