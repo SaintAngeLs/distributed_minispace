254a84f0fc498fe00d0fbd8b40b58daa581163c9
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-04 02:37:28 +0200

    (#342) web: create role command update

--- MiniSpace.Web/src/MiniSpace.Web/Areas/Organizations/CommandsDto/CreateOrganizationRoleCommand.cs
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/Organizations/CommandsDto/CreateOrganizationRoleCommand.cs
@@ -6,8 +6,17 @@ namespace MiniSpace.Web.Areas.Organizations.CommandsDto
 {
     public class CreateOrganizationRoleCommand
     {
-         public Guid OrganizationId { get; }
+        public Guid OrganizationId { get; }
         public string RoleName { get; }
+        public string Description { get; } 
         public Dictionary<string, bool> Permissions { get; }
+
+        public CreateOrganizationRoleCommand(Guid organizationId, string roleName, string description, Dictionary<string, bool> permissions)
+        {
+            OrganizationId = organizationId;
+            RoleName = roleName;
+            Description = description;
+            Permissions = permissions;
+        }
     }
 }
