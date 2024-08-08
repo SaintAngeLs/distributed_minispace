24b30480b17b7defba0e5341b6395fe4bc26c791
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-09 01:25:34 +0200

    (#333) events: add get evetns paginateg qury

---
+++ MiniSpace.Services.Events/src/MiniSpace.Services.Events.Application/Queries/GetPaginatedEvents.cs
@@ -0,0 +1,12 @@
+using Convey.CQRS.Queries;
+using MiniSpace.Services.Events.Application.DTO;
+using System.Collections.Generic;
+
+namespace MiniSpace.Services.Events.Application.Queries
+{
+    public class GetPaginatedEvents : IQuery<DTO.PagedResult<EventDto>>
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
