3f6e132c626f3fe83f648f8384b3fe3e430d9518
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-12 01:07:12 +0200

    (#369) organization: add new qury interface to get organizations followed by user

---
+++ MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Application/Queries/GetUserFollowOrganizations.cs
@@ -0,0 +1,17 @@
+using Convey.CQRS.Queries;
+using MiniSpace.Services.Organizations.Application.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace MiniSpace.Services.Organizations.Application.Queries
+{
+    public class GetUserFollowOrganizations : IQuery<IEnumerable<OrganizationGalleryUsersDto>>
+    {
+        public Guid UserId { get; set; }
+
+        public GetUserFollowOrganizations(Guid userId)
+        {
+            UserId = userId;
+        }
+    }
+}
