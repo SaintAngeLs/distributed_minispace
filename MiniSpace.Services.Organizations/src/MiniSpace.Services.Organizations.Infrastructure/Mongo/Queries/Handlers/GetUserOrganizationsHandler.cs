0cd4d3f5c8a63d8e04e87e8177b194f0d7dc8081
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-13 00:48:12 +0200

    (#369) organizations: update getuserOrganizations query handler

--- MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Infrastructure/Mongo/Queries/Handlers/GetUserOrganizationsHandler.cs
+++ MiniSpace.Services.Organizations/src/MiniSpace.Services.Organizations.Infrastructure/Mongo/Queries/Handlers/GetUserOrganizationsHandler.cs
@@ -12,16 +12,28 @@ namespace MiniSpace.Services.Organizations.Infrastructure.Mongo.Queries.Handlers
     public class GetUserOrganizationsHandler : IQueryHandler<GetUserOrganizations, IEnumerable<UserOrganizationsDto>>
     {
         private readonly IOrganizationRepository _organizationRepository;
+        private readonly IOrganizationMembersRepository _organizationMembersRepository;
 
-        public GetUserOrganizationsHandler(IOrganizationRepository organizationRepository)
+        public GetUserOrganizationsHandler(
+            IOrganizationRepository organizationRepository,
+            IOrganizationMembersRepository organizationMembersRepository)
         {
             _organizationRepository = organizationRepository;
+            _organizationMembersRepository = organizationMembersRepository;
         }
 
         public async Task<IEnumerable<UserOrganizationsDto>> HandleAsync(GetUserOrganizations query, CancellationToken cancellationToken)
         {
             var organizations = await _organizationRepository.GetOrganizationsByUserAsync(query.UserId);
-            return organizations.Select(org => new UserOrganizationsDto(org));
+            var organizationDtos = new List<UserOrganizationsDto>();
+
+            foreach (var org in organizations)
+            {
+                var users = await _organizationMembersRepository.GetMembersAsync(org.Id);
+                organizationDtos.Add(new UserOrganizationsDto(org, users));
+            }
+
+            return organizationDtos;
         }
     }
 }
