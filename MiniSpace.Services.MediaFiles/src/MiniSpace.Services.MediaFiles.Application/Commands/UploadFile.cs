fa9c67101f95de1a43ed1c1d0e1438fa2f26406b
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-14 16:58:56 +0200

    (#373) media files: update uplaod file command

--- MiniSpace.Services.MediaFiles/src/MiniSpace.Services.MediaFiles.Application/Commands/UploadFile.cs
+++ MiniSpace.Services.MediaFiles/src/MiniSpace.Services.MediaFiles.Application/Commands/UploadFile.cs
@@ -9,18 +9,20 @@ namespace MiniSpace.Services.MediaFiles.Application.Commands
         public Guid SourceId { get; set; }
         public string SourceType { get; set; }
         public Guid? OrganizationId { get; set; }
+        public Guid? EventId { get; set; }  
         public Guid UploaderId { get; set; }
         public string FileName { get; set; }
         public string FileContentType { get; set; }
         public byte[] FileData { get; set; }
 
-        public UploadFile(Guid fileId, Guid sourceId, string sourceType, Guid? organizationId,
+        public UploadFile(Guid fileId, Guid sourceId, string sourceType, Guid? organizationId, Guid? eventId,
             Guid uploaderId, string fileName, string fileContentType, byte[] fileData)
         {
             FileId = fileId == Guid.Empty ? Guid.NewGuid() : fileId;
             SourceId = sourceId;
             SourceType = sourceType;
             OrganizationId = organizationId;
+            EventId = eventId;
             UploaderId = uploaderId;
             FileName = fileName;
             FileContentType = fileContentType;
