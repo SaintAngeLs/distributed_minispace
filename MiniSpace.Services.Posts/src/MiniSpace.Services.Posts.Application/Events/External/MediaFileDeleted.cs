d477a6b1e37b0054e71afc723e73563ee35b83ce
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-14 21:52:17 +0200

    (#373) posts: udpate delete evnet handler

--- MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Events/External/MediaFileDeleted.cs
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Events/External/MediaFileDeleted.cs
@@ -4,17 +4,27 @@ using Convey.MessageBrokers;
 namespace MiniSpace.Services.Posts.Application.Events.External
 {
     [Message("mediafiles")]
-    public class MediaFileDeleted: IEvent
+    public class MediaFileDeleted : IEvent
     {
-        public Guid MediaFileId { get; }
+        public string MediaFileUrl { get; }
         public Guid SourceId { get; }
         public string Source { get; }
+        public Guid UploaderId { get; }
+        public Guid? OrganizationId { get; }
+        public Guid? EventId { get; }
+        public Guid? PostId { get; }
 
-        public MediaFileDeleted(Guid mediaFileId, Guid sourceId, string source)
+        public MediaFileDeleted(string mediaFileUrl, Guid sourceId, string source, 
+                                Guid uploaderId, Guid? organizationId,
+                                Guid? eventId = null, Guid? postId = null)
         {
-            MediaFileId = mediaFileId;
+            MediaFileUrl = mediaFileUrl;
             SourceId = sourceId;
             Source = source;
+            UploaderId = uploaderId;
+            OrganizationId = organizationId;
+            EventId = eventId;
+            PostId = postId;
         }
     }
 }
