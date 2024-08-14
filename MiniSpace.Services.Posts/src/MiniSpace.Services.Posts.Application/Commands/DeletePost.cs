bd35bfcb159f10b8d3e3b5c3ff40a6eaa1e84eda
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-14 16:19:20 +0200

    (#373) posts: update delelte posts handler

--- MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Commands/DeletePost.cs
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Commands/DeletePost.cs
@@ -1,11 +1,24 @@
 using Convey.CQRS.Commands;
+using MiniSpace.Services.Posts.Core.Entities;
+using System;
 
 namespace MiniSpace.Services.Posts.Application.Commands
 {
     public class DeletePost : ICommand
     {
         public Guid PostId { get; }
+        public Guid? UserId { get; }
+        public Guid? OrganizationId { get; }
+        public Guid? EventId { get; }
+        public string Context { get; }
 
-        public DeletePost(Guid postId) => PostId = postId;
-    }    
+        public DeletePost(Guid postId, Guid? userId, Guid? organizationId, Guid? eventId, string context)
+        {
+            PostId = postId;
+            UserId = userId;
+            OrganizationId = organizationId;
+            EventId = eventId;
+            Context = context;
+        }
+    }
 }
--- MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Commands/Handlers/ChangePostStateHandler.cs
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Commands/Handlers/ChangePostStateHandler.cs
@@ -5,39 +5,76 @@ using MiniSpace.Services.Posts.Application.Services;
 using MiniSpace.Services.Posts.Core.Entities;
 using MiniSpace.Services.Posts.Core.Exceptions;
 using MiniSpace.Services.Posts.Core.Repositories;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MiniSpace.Services.Posts.Application.Commands.Handlers
 {
     public class ChangePostStateHandler : ICommandHandler<ChangePostState>
     {
-        private readonly IPostRepository _postRepository;
+        private readonly IUserPostRepository _userPostRepository;
+        private readonly IOrganizationPostRepository _organizationPostRepository;
+        private readonly IUserEventPostRepository _userEventPostRepository;
+        private readonly IOrganizationEventPostRepository _organizationEventPostRepository;
         private readonly IAppContext _appContext;
         private readonly IDateTimeProvider _dateTimeProvider;
         private readonly IMessageBroker _messageBroker;
-        
-        public ChangePostStateHandler(IPostRepository postRepository, IAppContext appContext,
-            IDateTimeProvider dateTimeProvider, IMessageBroker messageBroker)
+
+        public ChangePostStateHandler(
+            IUserPostRepository userPostRepository,
+            IOrganizationPostRepository organizationPostRepository,
+            IUserEventPostRepository userEventPostRepository,
+            IOrganizationEventPostRepository organizationEventPostRepository,
+            IAppContext appContext,
+            IDateTimeProvider dateTimeProvider,
+            IMessageBroker messageBroker)
         {
-            _postRepository = postRepository;
+            _userPostRepository = userPostRepository;
+            _organizationPostRepository = organizationPostRepository;
+            _userEventPostRepository = userEventPostRepository;
+            _organizationEventPostRepository = organizationEventPostRepository;
             _appContext = appContext;
             _dateTimeProvider = dateTimeProvider;
             _messageBroker = messageBroker;
         }
-        
+
         public async Task HandleAsync(ChangePostState command, CancellationToken cancellationToken = default)
         {
-            var post = await _postRepository.GetAsync(command.PostId);
+            Post post = null;
+
+            // Determine which repository to use based on the context
+            if (command.Context == PostContext.UserPage)
+            {
+                post = await _userPostRepository.GetAsync(command.PostId);
+            }
+            else if (command.Context == PostContext.OrganizationPage)
+            {
+                post = await _organizationPostRepository.GetAsync(command.PostId);
+            }
+            else if (command.Context == PostContext.EventPage)
+            {
+                if (command.UserId.HasValue)
+                {
+                    post = await _userEventPostRepository.GetAsync(command.PostId);
+                }
+                else
+                {
+                    post = await _organizationEventPostRepository.GetAsync(command.PostId);
+                }
+            }
+
             if (post is null)
             {
                 throw new PostNotFoundException(command.PostId);
             }
-            
+
             var identity = _appContext.Identity;
-            if (identity.IsAuthenticated && identity.Id != post.OrganizerId && !identity.IsAdmin)
+            if (identity.IsAuthenticated && identity.Id != post.UserId && identity.Id != post.OrganizationId && !identity.IsAdmin)
             {
                 throw new UnauthorizedPostAccessException(command.PostId, identity.Id);
             }
-            
+
             if (!Enum.TryParse<State>(command.State, true, out var newState))
             {
                 throw new InvalidPostStateException(command.State);
@@ -47,12 +84,12 @@ namespace MiniSpace.Services.Posts.Application.Commands.Handlers
             {
                 throw new UnauthorizedPostOperationException(command.PostId, identity.Id);
             }
-            
+
             if (post.State == newState && post.State != State.ToBePublished)
             {
                 throw new PostStateAlreadySetException(post.Id, newState);
             }
-            
+
             var previousState = post.State.ToString().ToLowerInvariant();
             var now = _dateTimeProvider.Now;
 
@@ -74,10 +111,29 @@ namespace MiniSpace.Services.Posts.Application.Commands.Handlers
                 default:
                     throw new InvalidPostStateException(post.State.ToString().ToLowerInvariant());
             }
-            
-            await _postRepository.UpdateAsync(post);
+
+            // Update the post in the correct repository
+            if (command.Context == PostContext.UserPage)
+            {
+                await _userPostRepository.UpdateAsync(post);
+            }
+            else if (command.Context == PostContext.OrganizationPage)
+            {
+                await _organizationPostRepository.UpdateAsync(post);
+            }
+            else if (command.Context == PostContext.EventPage)
+            {
+                if (command.UserId.HasValue)
+                {
+                    await _userEventPostRepository.UpdateAsync(post);
+                }
+                else
+                {
+                    await _organizationEventPostRepository.UpdateAsync(post);
+                }
+            }
 
             await _messageBroker.PublishAsync(new PostStateChanged(post.Id, command.State, previousState));
         }
-    }    
+    }
 }
