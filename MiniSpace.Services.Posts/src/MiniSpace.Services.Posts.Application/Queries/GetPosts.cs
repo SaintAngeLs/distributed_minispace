c8f1d68f7b07b0b3e7916405779be6296d552ed1
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-15 00:41:44 +0200

    (#373) posts: add get posts query

--- MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Queries/GetPosts.cs
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Queries/GetPosts.cs
@@ -1,12 +1,17 @@
-using System.Diagnostics.CodeAnalysis;
 using Convey.CQRS.Queries;
 using MiniSpace.Services.Posts.Application.Dto;
+using MiniSpace.Services.Posts.Core.Wrappers;
 
 namespace MiniSpace.Services.Posts.Application.Queries
 {
-    [ExcludeFromCodeCoverage]
-    public class GetPosts : IQuery<IEnumerable<PostDto>>
+    public class GetPosts : IQuery<PagedResponse<PostDto>>
     {
-        public Guid EventId { get; set; }
+        public Guid? UserId { get; set; }
+        public Guid? OrganizationId { get; set; }
+        public Guid? EventId { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public IEnumerable<string> SortBy { get; set; } = new List<string> { "PublishDate" };
+        public string Direction { get; set; } = "asc";
     }
 }
