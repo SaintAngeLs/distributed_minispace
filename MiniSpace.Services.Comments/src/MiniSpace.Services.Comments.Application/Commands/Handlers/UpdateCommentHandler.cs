1e690c9d039131f485c532ed228eacbd83a70eb8
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-20 19:02:24 +0200

    (#331) reactions: update delelteComment Command handler

--- MiniSpace.Services.Comments/src/MiniSpace.Services.Comments.Application/Commands/Handlers/UpdateCommentHandler.cs
+++ MiniSpace.Services.Comments/src/MiniSpace.Services.Comments.Application/Commands/Handlers/UpdateCommentHandler.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Convey.CQRS.Commands;
@@ -13,15 +12,27 @@ namespace MiniSpace.Services.Comments.Application.Commands.Handlers
 {
     public class UpdateCommentHandler : ICommandHandler<UpdateComment>
     {
-        private readonly ICommentRepository _commentRepository;
+        private readonly IOrganizationEventsCommentRepository _organizationEventsCommentRepository;
+        private readonly IOrganizationPostsCommentRepository _organizationPostsCommentRepository;
+        private readonly IUserEventsCommentRepository _userEventsCommentRepository;
+        private readonly IUserPostsCommentRepository _userPostsCommentRepository;
         private readonly IAppContext _appContext;
         private readonly IMessageBroker _messageBroker;
         private readonly IDateTimeProvider _dateTimeProvider;
 
-        public UpdateCommentHandler(ICommentRepository commentRepository, IAppContext appContext,
-            IMessageBroker messageBroker, IDateTimeProvider dateTimeProvider)
+        public UpdateCommentHandler(
+            IOrganizationEventsCommentRepository organizationEventsCommentRepository,
+            IOrganizationPostsCommentRepository organizationPostsCommentRepository,
+            IUserEventsCommentRepository userEventsCommentRepository,
+            IUserPostsCommentRepository userPostsCommentRepository,
+            IAppContext appContext,
+            IMessageBroker messageBroker,
+            IDateTimeProvider dateTimeProvider)
         {
-            _commentRepository = commentRepository;
+            _organizationEventsCommentRepository = organizationEventsCommentRepository;
+            _organizationPostsCommentRepository = organizationPostsCommentRepository;
+            _userEventsCommentRepository = userEventsCommentRepository;
+            _userPostsCommentRepository = userPostsCommentRepository;
             _appContext = appContext;
             _messageBroker = messageBroker;
             _dateTimeProvider = dateTimeProvider;
@@ -29,22 +40,63 @@ namespace MiniSpace.Services.Comments.Application.Commands.Handlers
 
         public async Task HandleAsync(UpdateComment command, CancellationToken cancellationToken = default)
         {
-            var comment = await _commentRepository.GetAsync(command.CommentId);
+            Comment comment = null;
+
+            switch (command.CommentContext)
+            {
+                case nameof(CommentContext.OrganizationEvent):
+                    comment = await _organizationEventsCommentRepository.GetAsync(command.CommentId);
+                    break;
+
+                case nameof(CommentContext.OrganizationPost):
+                    comment = await _organizationPostsCommentRepository.GetAsync(command.CommentId);
+                    break;
+
+                case nameof(CommentContext.UserEvent):
+                    comment = await _userEventsCommentRepository.GetAsync(command.CommentId);
+                    break;
+
+                case nameof(CommentContext.UserPost):
+                    comment = await _userPostsCommentRepository.GetAsync(command.CommentId);
+                    break;
+
+                default:
+                    throw new InvalidCommentContextEnumException(command.CommentContext);
+            }
+
             if (comment is null)
             {
                 throw new CommentNotFoundException(command.CommentId);
             }
-            
+
             var identity = _appContext.Identity;
             if (identity.IsAuthenticated && identity.Id != comment.UserId)
             {
                 throw new UnauthorizedCommentAccessException(command.CommentId, identity.Id);
             }
-            
+
             comment.Update(command.TextContent, _dateTimeProvider.Now);
-            await _commentRepository.UpdateAsync(comment);
+
+            switch (command.CommentContext)
+            {
+                case nameof(CommentContext.OrganizationEvent):
+                    await _organizationEventsCommentRepository.UpdateAsync(comment);
+                    break;
+
+                case nameof(CommentContext.OrganizationPost):
+                    await _organizationPostsCommentRepository.UpdateAsync(comment);
+                    break;
+
+                case nameof(CommentContext.UserEvent):
+                    await _userEventsCommentRepository.UpdateAsync(comment);
+                    break;
+
+                case nameof(CommentContext.UserPost):
+                    await _userPostsCommentRepository.UpdateAsync(comment);
+                    break;
+            }
 
             await _messageBroker.PublishAsync(new CommentUpdated(command.CommentId));
         }
-    }    
+    }
 }
