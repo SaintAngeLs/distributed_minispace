ec2014d3dfef4e67d37db8578bae0d55fe4cf66f
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-23 01:51:42 +0200

    (#381) update create comment handler, update search comments quary

--- MiniSpace.Services.Comments/src/MiniSpace.Services.Comments.Application/Queries/SearchComments.cs
+++ MiniSpace.Services.Comments/src/MiniSpace.Services.Comments.Application/Queries/SearchComments.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Convey.CQRS.Queries;
 using MiniSpace.Services.Comments.Application.Dto;
 using MiniSpace.Services.Comments.Core.Wrappers;
@@ -10,6 +12,13 @@ namespace MiniSpace.Services.Comments.Application.Queries
         public Guid ContextId { get; set; }
         public string CommentContext { get; set; }
         public Guid? ParentId { get; set; }
-        public PageableDto Pageable { get; set; }
+        public int Page { get; set; }
+        public int Size { get; set; }
+        public string SortBy { get; set; }  // Changed to string to handle the incoming JSON properly
+        public string Direction { get; set; }
+
+        // This property will split the SortBy string into an array
+        public IEnumerable<string> SortByArray => 
+            string.IsNullOrEmpty(SortBy) ? new List<string> { "CreatedAt" } : SortBy.Split(',');
     }
 }
