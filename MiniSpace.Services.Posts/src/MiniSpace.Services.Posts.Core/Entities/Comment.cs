aa702db9ef7c379b8806e86bb0717102de5ea0ba
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-23 16:42:46 +0200

    (#351) posts: update core comment object

--- MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Core/Entities/Comment.cs
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Core/Entities/Comment.cs
@@ -4,27 +4,56 @@ namespace MiniSpace.Services.Posts.Core.Entities
 {
     public class Comment
     {
-        public Guid Id { get; private set; }
-        public Guid PostId { get; private set; }
-        public Guid UserId { get; private set; }
-        public string TextContent { get; private set; }
-        public DateTime CreatedAt { get; private set; }
-        public DateTime LastUpdatedAt { get; private set; }
-        
-        public Comment(Guid id, Guid postId, Guid userId, string textContent, DateTime createdAt)
+        public Guid Id { get; private set; }                  
+        public Guid ContextId { get; private set; }           
+        public string CommentContext { get; private set; }    
+        public Guid UserId { get; private set; }              
+        public Guid ParentId { get; private set; }            
+        public string TextContent { get; private set; }       
+        public DateTime CreatedAt { get; private set; }       
+        public DateTime LastUpdatedAt { get; private set; }   
+        public int RepliesCount { get; private set; }         
+        public bool IsDeleted { get; private set; }           
+
+        public Comment(Guid id, Guid contextId, string commentContext, Guid userId, 
+                       Guid parentId, string textContent, DateTime createdAt, 
+                       DateTime lastUpdatedAt, int repliesCount, bool isDeleted)
         {
             Id = id;
-            PostId = postId;
+            ContextId = contextId;
+            CommentContext = commentContext;
             UserId = userId;
+            ParentId = parentId;
             TextContent = textContent;
             CreatedAt = createdAt;
-            LastUpdatedAt = createdAt;
+            LastUpdatedAt = lastUpdatedAt;
+            RepliesCount = repliesCount;
+            IsDeleted = isDeleted;
         }
 
         public void UpdateText(string newText, DateTime updatedAt)
         {
             TextContent = newText;
             LastUpdatedAt = updatedAt;
+        }
+
+        public void MarkAsDeleted()
+        {
+            IsDeleted = true;
+            TextContent = "[deleted]";
+        }
+
+        public void IncrementRepliesCount()
+        {
+            RepliesCount++;
+        }
+
+        public void DecrementRepliesCount()
+        {
+            if (RepliesCount > 0)
+            {
+                RepliesCount--;
+            }
         }
     }
 }
