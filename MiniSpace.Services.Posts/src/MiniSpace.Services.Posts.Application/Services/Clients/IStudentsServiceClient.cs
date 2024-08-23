bcd028b150538108d952a5b4c7664cccf352e0e5
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-23 16:12:09 +0200

    (#351) posts: update dtos and service client

--- MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Services/Clients/IStudentsServiceClient.cs
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Services/Clients/IStudentsServiceClient.cs
@@ -1,9 +1,12 @@
-﻿using MiniSpace.Services.Posts.Application.Dto;
+﻿using System;
+using System.Threading.Tasks;
+using MiniSpace.Services.Posts.Application.Dto;
 
 namespace MiniSpace.Services.Posts.Application.Services.Clients
 {
     public interface IStudentsServiceClient
     {
         Task<StudentEventsDto> GetAsync(Guid id);
+        Task<UserDto> GetStudentByIdAsync(Guid studentId); 
     }
 }
