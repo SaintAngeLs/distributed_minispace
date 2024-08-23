9ed94b406b4122318c05966563b54cfa16852124
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-24 01:57:27 +0200

    (#388) web: update home razor page

--- MiniSpace.Web/src/MiniSpace.Web/Areas/Reactions/CommandDto/UpdateReactionDto.cs
+++ MiniSpace.Web/src/MiniSpace.Web/Areas/Reactions/CommandDto/UpdateReactionDto.cs
@@ -7,5 +7,7 @@ namespace MiniSpace.Web.Areas.Reactions.CommandDto
         public Guid ReactionId { get; set; }
         public Guid UserId { get; set; }
         public string NewReactionType { get; set; }
+        public string ContentType { get; set; }
+        public string TargetType { get; set; }
     }
 }
