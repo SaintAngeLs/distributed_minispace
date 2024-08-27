8f45f866495691bdc0ad053504ac08d86982f36b
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-27 19:08:52 +0200

    (#394) communication: update the core chat object

--- MiniSpace.Services.Communication/src/MiniSpace.Services.Communication.Core/Entities/Chat.cs
+++ MiniSpace.Services.Communication/src/MiniSpace.Services.Communication.Core/Entities/Chat.cs
@@ -6,11 +6,20 @@ namespace MiniSpace.Services.Communication.Core.Entities
         public List<Guid> ParticipantIds { get; private set; }
         public List<Message> Messages { get; private set; }
 
+        // Constructor for creating a new chat with a new Id
         public Chat(List<Guid> participantIds)
         {
             Id = Guid.NewGuid();
             ParticipantIds = participantIds;
             Messages = new List<Message>();
+        }
+
+        // Constructor for loading an existing chat from the database
+        public Chat(Guid id, List<Guid> participantIds, List<Message> messages)
+        {
+            Id = id;
+            ParticipantIds = participantIds;
+            Messages = messages;
         }
 
         public void AddMessage(Message message)
