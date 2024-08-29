fd8b17b6b47fe4589b048cac20556e0f8ff7da81
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-30 00:20:55 +0200

    (#397) friends: update api

--- MiniSpace.Services.Friends/src/MiniSpace.Services.Friends.Api/Program.cs
+++ MiniSpace.Services.Friends/src/MiniSpace.Services.Friends.Api/Program.cs
@@ -48,9 +48,6 @@ namespace MiniSpace.Services.Friends.Api
                         .Get<GetFollowers, PagedResponse<UserFriendsDto>>("friends/{userId}/followers")
                         .Get<GetFollowing, PagedResponse<UserFriendsDto>>("friends/{userId}/following")
 
-
-
-
                         .Put<SentFriendRequestWithdraw>("friends/requests/{userId}/withdraw", afterDispatch: (cmd, ctx) => ctx.Response.Ok())
 
                         
