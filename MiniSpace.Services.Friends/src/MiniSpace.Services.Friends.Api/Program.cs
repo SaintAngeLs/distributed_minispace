3c45fd50321d2f44dc562df633240300274ab1b1
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-08-30 00:17:53 +0200

    (#397) friends; udpate api

--- MiniSpace.Services.Friends/src/MiniSpace.Services.Friends.Api/Program.cs
+++ MiniSpace.Services.Friends/src/MiniSpace.Services.Friends.Api/Program.cs
@@ -45,6 +45,10 @@ namespace MiniSpace.Services.Friends.Api
                         .Get<GetFriendRequests, PagedResponse<FriendRequestDto>>("friends/pending/all")
                         .Get<GetSentFriendRequests, PagedResponse<UserRequestsDto>>("friends/requests/sent/{userId}")
 
+                        .Get<GetFollowers, PagedResponse<UserFriendsDto>>("friends/{userId}/followers")
+                        .Get<GetFollowing, PagedResponse<UserFriendsDto>>("friends/{userId}/following")
+
+
 
 
                         .Put<SentFriendRequestWithdraw>("friends/requests/{userId}/withdraw", afterDispatch: (cmd, ctx) => ctx.Response.Ok())
