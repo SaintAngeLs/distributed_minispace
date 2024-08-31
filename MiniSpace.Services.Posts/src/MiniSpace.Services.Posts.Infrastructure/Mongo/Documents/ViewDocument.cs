873d50bff76cd4193b3998ed398bfdf36f1b611a
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-01 01:48:38 +0200

    (#400) posts: add views documents

---
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Infrastructure/Mongo/Documents/ViewDocument.cs
@@ -0,0 +1,28 @@
+using System;
+using Convey.Types;
+using MiniSpace.Services.Posts.Core.Entities;
+
+namespace MiniSpace.Services.Posts.Infrastructure.Mongo.Documents
+{
+    public class ViewDocument : IIdentifiable<Guid>
+    {
+        public Guid Id { get; set; }
+        public Guid PostId { get; set; }
+        public DateTime Date { get; set; }
+
+        public static ViewDocument FromEntity(View view)
+        {
+            return new ViewDocument
+            {
+                Id = Guid.NewGuid(),
+                PostId = view.PostId,
+                Date = view.Date
+            };
+        }
+
+        public View ToEntity()
+        {
+            return new View(PostId, Date);
+        }
+    }
+}
