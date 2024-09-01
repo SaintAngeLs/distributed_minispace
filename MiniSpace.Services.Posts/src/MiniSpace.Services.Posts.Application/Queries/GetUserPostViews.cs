2e68da6a726523182a320a2a0e6b305e96a1c0d3
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-01 02:59:47 +0200

    (#400) posts: add query

---
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Queries/GetUserPostViews.cs
@@ -0,0 +1,22 @@
+using System;
+using Convey.CQRS.Queries;
+using MiniSpace.Services.Posts.Application.Dto;
+using MiniSpace.Services.Posts.Application.DTO;
+using MiniSpace.Services.Posts.Core.Wrappers;
+
+namespace MiniSpace.Services.Posts.Application.Queries
+{
+    public class GetUserPostViews : IQuery<PagedResponse<ViewDto>>
+    {
+        public Guid UserId { get; }
+        public int PageNumber { get; }
+        public int PageSize { get; }
+
+        public GetUserPostViews(Guid userId, int pageNumber = 1, int pageSize = 10)
+        {
+            UserId = userId;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
