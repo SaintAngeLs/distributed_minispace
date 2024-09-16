baa9d5ddb005b830fd6d3e2d6d1adf0706f7eaf6
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-16 18:23:24 +0200

    (#411) posts: udpate post created event

--- MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Events/PostCreated.cs
+++ MiniSpace.Services.Posts/src/MiniSpace.Services.Posts.Application/Events/PostCreated.cs
@@ -1,16 +1,31 @@
 using Convey.CQRS.Events;
+using System;
+using System.Collections.Generic;
 
 namespace MiniSpace.Services.Posts.Application.Events
 {
     public class PostCreated : IEvent
     {
         public Guid PostId { get; }
-        public IEnumerable<string> MediaFilesUrls { get; }
+        public Guid? UserId { get; }  
+        public Guid? OrganizationId { get; }  
+        public Guid? EventId { get; }  
+        public string TextContent { get; }  
+        public IEnumerable<string> MediaFilesUrls { get; }  
+        public string Context { get; }  
+        public string Visibility { get; }  
 
-        public PostCreated(Guid postId, IEnumerable<string> mediaFilesUrls)
+        public PostCreated(Guid postId, Guid? userId, Guid? organizationId, Guid? eventId, string textContent,
+            IEnumerable<string> mediaFilesUrls, string context, string visibility)
         {
             PostId = postId;
+            UserId = userId;
+            OrganizationId = organizationId;
+            EventId = eventId;
+            TextContent = textContent;
             MediaFilesUrls = mediaFilesUrls;
+            Context = context;
+            Visibility = visibility;
         }
     }
 }
