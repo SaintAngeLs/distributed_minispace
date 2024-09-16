e630f92350c5aec274bcca4047f705557fb2baec
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-16 18:37:44 +0200

    (#412) notifications: update post created event handler

--- MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Events/External/Posts/Handlers/PostCreatedHandler.cs
+++ MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Events/External/Posts/Handlers/PostCreatedHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Linq;
 using Convey.CQRS.Events;
 using MiniSpace.Services.Notifications.Application.Services.Clients;
 using MiniSpace.Services.Notifications.Core.Repositories;
@@ -10,193 +11,137 @@ using MiniSpace.Services.Notifications.Application.Services;
 using Microsoft.AspNetCore.SignalR;
 using MiniSpace.Services.Notifications.Application.Hubs;
 using Microsoft.Extensions.Logging;
-using MiniSpace.Services.Notifications.Application.Dto;
-using MiniSpace.Services.Notifications.Application.Dto.Events;
-using MiniSpace.Services.Notifications.Application.Dto.Posts;
 
 namespace MiniSpace.Services.Notifications.Application.Events.External.Handlers
 {
     public class PostCreatedHandler : IEventHandler<PostCreated>
     {
-        private readonly IMessageBroker _messageBroker;
-        private readonly IUserNotificationsRepository _studentNotificationsRepository;
-        private readonly IEventsServiceClient _eventsServiceClient;
-        private readonly IPostsServiceClient _postsServiceClient;
-        private readonly IStudentsServiceClient _studentsServiceClient;
+        private readonly IFriendsServiceClient _friendsServiceClient;
+        private readonly IOrganizationsServiceClient _organizationsServiceClient;
+        private readonly IUserNotificationsRepository _userNotificationsRepository;
         private readonly IHubContext<NotificationHub> _hubContext;
         private readonly ILogger<PostCreatedHandler> _logger;
 
         public PostCreatedHandler(
-            IMessageBroker messageBroker,
-            IUserNotificationsRepository studentNotificationsRepository,
-            IEventsServiceClient eventsServiceClient,
-            IPostsServiceClient postsServiceClient,
-            IStudentsServiceClient studentsServiceClient,
+            IFriendsServiceClient friendsServiceClient,
+            IOrganizationsServiceClient organizationsServiceClient,
+            IUserNotificationsRepository userNotificationsRepository,
             IHubContext<NotificationHub> hubContext,
             ILogger<PostCreatedHandler> logger)
         {
-            _messageBroker = messageBroker;
-            _studentNotificationsRepository = studentNotificationsRepository;
-            _eventsServiceClient = eventsServiceClient;
-            _postsServiceClient = postsServiceClient;
-            _studentsServiceClient = studentsServiceClient;
+            _friendsServiceClient = friendsServiceClient;
+            _organizationsServiceClient = organizationsServiceClient;
+            _userNotificationsRepository = userNotificationsRepository;
             _hubContext = hubContext;
             _logger = logger;
         }
 
         public async Task HandleAsync(PostCreated eventArgs, CancellationToken cancellationToken)
         {
-            var post = await _postsServiceClient.GetPostAsync(eventArgs.PostId);
-            if (post == null)
+            if (!eventArgs.ShouldNotify)
             {
-                _logger.LogError("Post not found.");
+                _logger.LogInformation("PostCreated event received, but notifications are disabled.");
                 return;
             }
 
-            var eventDetails = await _eventsServiceClient.GetEventAsync(post.EventId ?? post.OrganizationId ?? post.UserId ?? post.Id);
-            if (eventDetails == null)
+            if (eventArgs.UserId.HasValue)
             {
-                _logger.LogError("Event not found for the post.");
-                return;
+                await NotifyFriendsAndFollowersAsync(eventArgs);
             }
-
-            var eventParticipants = await _eventsServiceClient.GetParticipantsAsync(post.EventId ?? post.OrganizationId ?? post.UserId ?? post.Id);
-            if (eventParticipants == null)
+            else if (eventArgs.OrganizationId.HasValue)
             {
-                _logger.LogError("No participants found for the event.");
-                return;
+                await NotifyOrganizationMembersAsync(eventArgs);
             }
-
-            foreach (var studentParticipant in eventParticipants.InterestedStudents)
-            {
-                var student = await _studentsServiceClient.GetAsync(studentParticipant.UserId);
-                if (student != null)
-                {
-                    await NotifyStudent(student, eventDetails, post);
-                }
-            }
-
-            foreach (var studentParticipant in eventParticipants.SignedUpStudents)
-            {
-                var student = await _studentsServiceClient.GetAsync(studentParticipant.UserId);
-                if (student != null)
-                {
-                    await NotifyStudent(student, eventDetails, post);
-                }
-            }
-
-            // Notify the organizer
-            await NotifyOrganizer(eventDetails.Organizer, eventDetails, post);
         }
 
-        private async Task NotifyStudent(UserDto student, EventDto eventDetails, PostDto post)
+        private async Task NotifyFriendsAndFollowersAsync(PostCreated eventArgs)
         {
-            var notificationMessage = $"A new post has been created for an event you are interested in: '{eventDetails.Name}'.";
-            var detailsHtml = $"<p>{notificationMessage} Check out the post details here: {post.TextContent}</p>";
+            try
+            {
+                // Get the user's friends
+                var friends = await _friendsServiceClient.GetFriendsAsync(eventArgs.UserId.Value);
+                var friendIds = friends.Select(f => f.FriendId).ToList();
+
+                // Get the user's followers
+                var followers = await _friendsServiceClient.GetRequestsAsync(eventArgs.UserId.Value);
+                var followerIds = followers.Select(f => f.InviterId).ToList();
+
+                // Combine friends and followers into one list of user IDs to notify
+                var userIdsToNotify = friendIds.Concat(followerIds).Distinct().ToList();
+
+                foreach (var userId in userIdsToNotify)
+                {
+                    var notificationMessage = $"A new post has been created by your friend or someone you follow (Post ID: {eventArgs.PostId}).";
+                    await CreateAndSendNotification(userId, eventArgs, notificationMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to notify user's friends and followers: {ex.Message}");
+            }
+        }
+
+        private async Task NotifyOrganizationMembersAsync(PostCreated eventArgs)
+        {
+            try
+            {
+                var organizationMembers = await _organizationsServiceClient.GetOrganizationMembersAsync(eventArgs.OrganizationId.Value);
+
+                if (organizationMembers != null && organizationMembers.Users != null)
+                {
+                    foreach (var member in organizationMembers.Users)
+                    {
+                        var notificationMessage = $"A new post has been created by your organization (Post ID: {eventArgs.PostId}).";
+                        await CreateAndSendNotification(member.Id, eventArgs, notificationMessage);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to notify organization members: {ex.Message}");
+            }
+        }
+
+        private async Task CreateAndSendNotification(Guid userId, PostCreated eventArgs, string notificationMessage)
+        {
+            var detailsHtml = $"<p>{notificationMessage} Check out the post details here: {eventArgs.TextContent}</p>";
 
             var notification = new Notification(
                 notificationId: Guid.NewGuid(),
-                userId: student.Id,
+                userId: userId,
                 message: notificationMessage,
                 status: NotificationStatus.Unread,
                 createdAt: DateTime.UtcNow,
                 updatedAt: null,
-                relatedEntityId: post.EventId ?? post.OrganizationId ?? post.UserId ?? post.Id,
+                relatedEntityId: eventArgs.PostId,
                 eventType: NotificationEventType.PostCreated,
                 details: detailsHtml
             );
 
-            _logger.LogInformation($"Creating post creation notification for user: {student.Id}");
+            _logger.LogInformation($"Creating post creation notification for user: {userId}");
 
-            var studentNotifications = await _studentNotificationsRepository.GetByUserIdAsync(student.Id);
-            if (studentNotifications == null)
+            var userNotifications = await _userNotificationsRepository.GetByUserIdAsync(userId);
+            if (userNotifications == null)
             {
-                studentNotifications = new UserNotifications(student.Id);
+                userNotifications = new UserNotifications(userId);
             }
 
-            studentNotifications.AddNotification(notification);
-            await _studentNotificationsRepository.AddOrUpdateAsync(studentNotifications);
-
-            var notificationCreatedEvent = new NotificationCreated(
-                notification.NotificationId,
-                student.Id,
-                notificationMessage,
-                DateTime.UtcNow,
-                NotificationEventType.PostCreated.ToString(),
-                post.EventId ?? post.OrganizationId ?? post.UserId ?? post.Id,
-                detailsHtml
-            );
-
-            await _messageBroker.PublishAsync(notificationCreatedEvent);
+            userNotifications.AddNotification(notification);
+            await _userNotificationsRepository.AddOrUpdateAsync(userNotifications);
 
             var notificationDto = new NotificationDto
             {
-                UserId = student.Id,
+                UserId = userId,
                 Message = notificationMessage,
                 CreatedAt = DateTime.UtcNow,
                 EventType = NotificationEventType.PostCreated,
-                RelatedEntityId = post.EventId,
+                RelatedEntityId = eventArgs.PostId,
                 Details = detailsHtml
             };
 
             // Broadcast SignalR notification
             await NotificationHub.BroadcastNotification(_hubContext, notificationDto, _logger);
-            _logger.LogInformation($"Broadcasted SignalR notification to student with ID {student.Id}.");
-        }
-
-        private async Task NotifyOrganizer(OrganizerDto organizer, EventDto eventDetails, PostDto post)
-        {
-            var notificationMessage = $"A new post has been created for your event '{eventDetails.Name}'.";
-            var detailsHtml = $"<p>{notificationMessage} View the new post here: {post.TextContent}</p>";
-
-            var notification = new Notification(
-                notificationId: Guid.NewGuid(),
-                userId: organizer.Id,
-                message: notificationMessage,
-                status: NotificationStatus.Unread,
-                createdAt: DateTime.UtcNow,
-                updatedAt: null,
-                relatedEntityId: post.EventId ?? post.OrganizationId ?? post.UserId ?? post.Id,
-                eventType: NotificationEventType.PostCreated,
-                details: detailsHtml
-            );
-
-            _logger.LogInformation($"Creating post creation notification for organizer: {organizer.Id}");
-
-            var organizerNotifications = await _studentNotificationsRepository.GetByUserIdAsync(organizer.Id);
-            if (organizerNotifications == null)
-            {
-                organizerNotifications = new UserNotifications(organizer.Id);
-            }
-
-            organizerNotifications.AddNotification(notification);
-            await _studentNotificationsRepository.AddOrUpdateAsync(organizerNotifications);
-
-            var notificationCreatedEvent = new NotificationCreated(
-                notification.NotificationId,
-                organizer.Id,
-                notificationMessage,
-                DateTime.UtcNow,
-                NotificationEventType.PostCreated.ToString(),
-                post.EventId ?? post.OrganizationId ?? post.UserId ?? post.Id,
-                detailsHtml
-            );
-
-            await _messageBroker.PublishAsync(notificationCreatedEvent);
-
-            var notificationDto = new NotificationDto
-            {
-                UserId = organizer.Id,
-                Message = notificationMessage,
-                CreatedAt = DateTime.UtcNow,
-                EventType = NotificationEventType.PostCreated,
-                RelatedEntityId = post.EventId,
-                Details = detailsHtml
-            };
-
-            // Broadcast SignalR notification
-            await NotificationHub.BroadcastNotification(_hubContext, notificationDto, _logger);
-            _logger.LogInformation($"Broadcasted SignalR notification to organizer with ID {organizer.Id}.");
+            _logger.LogInformation($"Broadcasted SignalR notification to user with ID {userId}.");
         }
     }
 }
