8aa77fd48f2078380a19bf29a295277f8371e2da
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-16 18:45:17 +0200

    (#412) notifications: update post updated evnet

--- MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Events/External/Posts/PostUpdated.cs
+++ MiniSpace.Services.Notifications/src/MiniSpace.Services.Notifications.Application/Events/External/Posts/PostUpdated.cs
@@ -1,5 +1,6 @@
 using Convey.CQRS.Events;
 using Convey.MessageBrokers;
+using System;
 
 namespace MiniSpace.Services.Notifications.Application.Events.External
 {
@@ -7,10 +8,23 @@ namespace MiniSpace.Services.Notifications.Application.Events.External
     public class PostUpdated : IEvent
     {
         public Guid PostId { get; }
+        public Guid? UserId { get; }
+        public Guid? OrganizationId { get; }
+        public Guid? EventId { get; }
+        public string TextContent { get; }
+        public string Context { get; }
+        public bool ShouldNotify { get; }
 
-        public PostUpdated(Guid postId)
+        public PostUpdated(Guid postId, Guid? userId, Guid? organizationId, 
+            Guid? eventId, string textContent, string context, bool shouldNotify)
         {
             PostId = postId;
+            UserId = userId;
+            OrganizationId = organizationId;
+            EventId = eventId;
+            TextContent = textContent;
+            Context = context;
+            ShouldNotify = shouldNotify;
         }
     }
 }
