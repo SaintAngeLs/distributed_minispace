85392501834de6fcf7934372645d0e52c517f384
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-10-11 18:31:17 +0200

    (#433) users: update notificationPreference dto

--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Dto/NotificationPreferencesDto.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Dto/NotificationPreferencesDto.cs
@@ -7,40 +7,60 @@ namespace MiniSpace.Services.Students.Application.Dto
     public class NotificationPreferencesDto
     {
         public Guid StudentId { get; set; }
-        public bool AccountChanges { get; set; }
         public bool SystemLogin { get; set; }
-        public bool NewEvent { get; set; }
         public bool InterestBasedEvents { get; set; }
         public bool EventNotifications { get; set; }
         public bool CommentsNotifications { get; set; }
         public bool PostsNotifications { get; set; }
-        public bool FriendsNotifications { get; set; }
+
+        public bool EventRecommendation { get; set; }
+        public bool FriendsRecommendation { get; set; }
+        public bool FriendsPosts { get; set; }
+        public bool PostsRecommendation { get; set; }
+        public bool EventsIAmInterestedInNotification { get; set; }
+        public bool EventsIAmSignedUpToNotification { get; set; }
+        public bool PostsOfPeopleIFollowNotification { get; set; }
+        public bool EventNotificationForPeopleIFollow { get; set; }
 
         public NotificationPreferencesDto()
         {
-            AccountChanges = false;
             SystemLogin = false;
-            NewEvent = false;
             InterestBasedEvents = false;
             EventNotifications = false;
             CommentsNotifications = false;
             PostsNotifications = false;
-            FriendsNotifications = false;
+
+            EventRecommendation = false;
+            FriendsRecommendation = false;
+            FriendsPosts = false;
+            PostsRecommendation = false;
+            EventsIAmInterestedInNotification = false;
+            EventsIAmSignedUpToNotification = false;
+            PostsOfPeopleIFollowNotification = false;
+            EventNotificationForPeopleIFollow = false;
         }
 
-        public NotificationPreferencesDto(Guid studentId, bool accountChanges, bool systemLogin, bool newEvent, bool interestBasedEvents,
-                                          bool eventNotifications, bool commentsNotifications, bool postsNotifications,
-                                          bool friendsNotifications)
+        public NotificationPreferencesDto(Guid studentId, bool systemLogin, bool interestBasedEvents, bool eventNotifications,
+                                          bool commentsNotifications, bool postsNotifications, bool eventRecommendation,
+                                          bool friendsRecommendation, bool friendsPosts, bool postsRecommendation,
+                                          bool eventsIAmInterestedInNotification, bool eventsIAmSignedUpToNotification,
+                                          bool postsOfPeopleIFollowNotification, bool eventNotificationForPeopleIFollow)
         {
             StudentId = studentId;
-            AccountChanges = accountChanges;
             SystemLogin = systemLogin;
-            NewEvent = newEvent;
             InterestBasedEvents = interestBasedEvents;
             EventNotifications = eventNotifications;
             CommentsNotifications = commentsNotifications;
             PostsNotifications = postsNotifications;
-            FriendsNotifications = friendsNotifications;
+
+            EventRecommendation = eventRecommendation;
+            FriendsRecommendation = friendsRecommendation;
+            FriendsPosts = friendsPosts;
+            PostsRecommendation = postsRecommendation;
+            EventsIAmInterestedInNotification = eventsIAmInterestedInNotification;
+            EventsIAmSignedUpToNotification = eventsIAmSignedUpToNotification;
+            PostsOfPeopleIFollowNotification = postsOfPeopleIFollowNotification;
+            EventNotificationForPeopleIFollow = eventNotificationForPeopleIFollow;
         }
     }
 }
