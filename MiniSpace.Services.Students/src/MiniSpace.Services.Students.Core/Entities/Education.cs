5c98bd9a7bc9c9889e9869218dc8d84c91f7025b
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-10-12 22:22:23 +0200

    (#433) users: add organizationId to core domatin aggragates

--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Core/Entities/Education.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Core/Entities/Education.cs
@@ -2,15 +2,16 @@ namespace MiniSpace.Services.Students.Core.Entities
 {
     public class Education
     {
+        public Guid OrganizationId { get; set; }
         public string InstitutionName { get; set; }
         public string Degree { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Description { get; set; }
-        public Guid OrganizationId { get; set; }
-
-        public Education(string institutionName, string degree, DateTime startDate, DateTime endDate, string description)
+        
+        public Education(Guid organizationId, string institutionName, string degree, DateTime startDate, DateTime endDate, string description)
         {
+            OrganizationId = organizationId;
             InstitutionName = institutionName;
             Degree = degree;
             StartDate = startDate;
--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Core/Entities/Work.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Core/Entities/Work.cs
@@ -2,15 +2,16 @@ namespace MiniSpace.Services.Students.Core.Entities
 {
     public class Work
     {
+        public Guid OrganizationId { get; set; }
         public string Company { get; set; }
         public string Position { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Description { get; set; }
-        public Guid OrganizationId { get; set; }
 
-        public Work(string company, string position, DateTime startDate, DateTime endDate, string description)
+        public Work(Guid organizationId, string company, string position, DateTime startDate, DateTime endDate, string description)
         {
+            OrganizationId = organizationId;
             Company = company;
             Position = position;
             StartDate = startDate;
