494711070ae22881066c779cb0f4dd26f4456489
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-10-15 16:54:34 +0200

    (#436) users: udpate available settings dto

--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Dto/AvailableSettingsDto.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Application/Dto/AvailableSettingsDto.cs
@@ -15,6 +15,23 @@ namespace MiniSpace.Services.Students.Application.Dto
         public string InterestsVisibility { get; set; }
         public string ContactEmailVisibility { get; set; }
         public string PhoneNumberVisibility { get; set; }
+        public string ProfileImageVisibility { get; set; }
+        public string BannerImageVisibility { get; set; }
+        public string GalleryVisibility { get; set; }
+        public string ConnectionVisibility { get; set; }
+        public string FollowersVisibility { get; set; }
+        public string FollowingVisibility { get; set; }
+        public string MyPostsVisibility { get; set; }
+        public string ConnectionsPostsVisibility { get; set; }
+        public string MyRepostsVisibility { get; set; }
+        public string RepostsOfMyConnectionsVisibility { get; set; }
+        public string OrganizationIAmCreatorVisibility { get; set; }
+        public string OrganizationIFollowVisibility { get; set; }
+        public string IsOnlineVisibility { get; set; }
+        public string DeviceTypeVisibility { get; set; }
+        public string LastActiveVisibility { get; set; }
+        public string CountryVisibility { get; set; }
+        public string CityVisibility { get; set; }
         public string PreferredLanguage { get; set; }
         public string FrontendVersion { get; set; }
     }
