5d7ee5d17a1d4e8da918185b5768dba1fba3d490
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-10-15 02:06:15 +0200

    (#435) users: update repository core

--- MiniSpace.Services.Students/src/MiniSpace.Services.Students.Core/Repositories/IStudentRepository.cs
+++ MiniSpace.Services.Students/src/MiniSpace.Services.Students.Core/Repositories/IStudentRepository.cs
@@ -5,6 +5,7 @@ namespace MiniSpace.Services.Students.Core.Repositories
     public interface IStudentRepository
     {
         Task<Student> GetAsync(Guid id);
+        Task<List<Student>> GetStudentsByEventIdAsync(Guid eventId);
         Task AddAsync(Student student);
         Task UpdateAsync(Student student);
         Task DeleteAsync(Guid id);
