04983be0c029ea404a5558a13ac77b65c1244b4b
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-10-17 16:31:50 +0200

    (#435) identity: add DeviceInfo core domain aggregate

---
+++ MiniSpace.Services.Identity/src/MiniSpace.Services.Identity.Core/Entities/DeviceInfo.cs
@@ -0,0 +1,8 @@
+namespace MiniSpace.Services.Identity.Core.Entities
+{
+    public class DeviceInfo
+    {
+        public string DeviceType { get; set; }
+        public string UserAgent { get; set; }
+    }
+}
