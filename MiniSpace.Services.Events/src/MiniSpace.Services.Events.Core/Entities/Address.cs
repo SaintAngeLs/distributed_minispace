3a232c92a8c3592ad38fa5bc42f7367c10d51a5a
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-10-28 00:09:39 +0100

    (#432) events: update Event core for lat long

--- MiniSpace.Services.Events/src/MiniSpace.Services.Events.Core/Entities/Address.cs
+++ MiniSpace.Services.Events/src/MiniSpace.Services.Events.Core/Entities/Address.cs
@@ -1,32 +1,44 @@
 ﻿namespace MiniSpace.Services.Events.Core.Entities
 {
-    public class Address(
-        string buildingName,
-        string street,
-        string buildingNumber,
-        string apartmentNumber,
-        string city,
-        string zipCode,
-        string country) 
+    public class Address
     {
-        public string BuildingName { get; set; } = buildingName;
-        public string Street { get; set; } = street;
-        public string BuildingNumber { get; set; } = buildingNumber;
-        public string ApartmentNumber { get; set; } = apartmentNumber;
-        public string City { get; set; } = city;
-        public string ZipCode { get; set; } = zipCode;
-        public string Country { get; set; } = country; 
+        public string BuildingName { get; private set; }
+        public string Street { get; private set; }
+        public string BuildingNumber { get; private set; }
+        public string ApartmentNumber { get; private set; }
+        public string City { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Country { get; private set; }
+        public double Latitude { get; private set; }  
+        public double Longitude { get; private set; } 
 
-        public Address Update(string buildingName, string street, string buildingNumber, string apartmentNumber,
-            string city, string zipCode, string country) 
+        public Address(string buildingName, string street, string buildingNumber, string apartmentNumber, 
+            string city, string zipCode, string country, double latitude, double longitude)
         {
-            BuildingName = buildingName == string.Empty ? BuildingName : buildingName;
-            Street = street == string.Empty ? Street : street;
-            BuildingNumber = buildingNumber == string.Empty ? BuildingNumber : buildingNumber;
-            ApartmentNumber = apartmentNumber == string.Empty ? ApartmentNumber : apartmentNumber;
-            City = city == string.Empty ? City : city;
-            ZipCode = zipCode == string.Empty ? ZipCode : zipCode;
-            Country = country == string.Empty ? Country : country; // Update Country if provided
+            BuildingName = buildingName;
+            Street = street;
+            BuildingNumber = buildingNumber;
+            ApartmentNumber = apartmentNumber;
+            City = city;
+            ZipCode = zipCode;
+            Country = country;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public Address Update(string buildingName, string street, string buildingNumber, string apartmentNumber, 
+            string city, string zipCode, string country, double? latitude, double? longitude)
+        {
+            BuildingName = !string.IsNullOrWhiteSpace(buildingName) ? buildingName : BuildingName;
+            Street = !string.IsNullOrWhiteSpace(street) ? street : Street;
+            BuildingNumber = !string.IsNullOrWhiteSpace(buildingNumber) ? buildingNumber : BuildingNumber;
+            ApartmentNumber = !string.IsNullOrWhiteSpace(apartmentNumber) ? apartmentNumber : ApartmentNumber;
+            City = !string.IsNullOrWhiteSpace(city) ? city : City;
+            ZipCode = !string.IsNullOrWhiteSpace(zipCode) ? zipCode : ZipCode;
+            Country = !string.IsNullOrWhiteSpace(country) ? country : Country;
+            Latitude = latitude ?? Latitude; 
+            Longitude = longitude ?? Longitude; 
+
             return this;
         }
     }
