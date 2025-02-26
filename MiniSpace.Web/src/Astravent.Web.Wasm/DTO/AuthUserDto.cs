8638c0acf0a7160ce95522dee1a9eb978d307250
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2025-02-26 20:23:59 +0100

    #441 nuar: refactor services in wasm app

--- MiniSpace.Web/src/Astravent.Web.Wasm/DTO/AuthUserDto.cs
+++ MiniSpace.Web/src/Astravent.Web.Wasm/DTO/AuthUserDto.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Astravent.Web.Wasm.DTO
 {
-    public class UserDto
+    public class AuthUserDto
     {   
         public Guid Id { get; set; }
         public string Name { get; set; }
